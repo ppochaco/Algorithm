11dfbba4fea3a9f319f2f1c91dacaa39236e0df9
A_jin <77830408+ppocchi@users.noreply.github.com>
2023-07-19 17:42:29 +0900

    [Silver I] Title: 단지번호붙이기, Time: 68 ms, Memory: 34176 KB -BaekjoonHub

---
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -0,0 +1,51 @@
+import sys
+input = sys.stdin.readline
+
+from collections import deque
+
+n = int(input())
+graph = [[0]*0 for _ in range(n)]
+visited = [[False]*n for _ in range(n)]
+direction = [[1,0], [0,1], [-1,0], [0,-1]]
+queue = []
+cnt = 0
+block = []
+
+def bfs(i, j):
+    home = 1
+    queue = deque([(i,j)])
+    visited[i][j] = True
+
+    while queue:
+        v = queue.popleft()
+
+        for k in range(4):
+            cur_i = int(v[0]) + direction[k][0]
+            cur_j = int(v[1]) + direction[k][1]
+
+            if(cur_i < 0 or cur_i >= n):
+                continue
+            if(cur_j < 0 or cur_j >= n):
+                continue
+
+            if graph[cur_i][cur_j] == 1 and visited[cur_i][cur_j] == False:
+                home += 1
+                queue.append((cur_i, cur_j))
+                visited[cur_i][cur_j] = True
+
+    block.append(home)
+
+for i in range(n):
+    temp = list(input())
+    for j in range(n):
+        graph[i].append(int(temp[j]))
+
+for i in range(n):
+    for j in range(n):
+        if graph[i][j] == 1 and visited[i][j] == False:
+            cnt += 1
+            bfs(i,j)
+
+print(cnt)
+block.sort()
+print(*block, sep='\n')
