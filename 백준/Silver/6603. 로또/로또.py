245c52b5c3a1f4792cdc3ca6110397f6e178636c
A_jin <77830408+ppocchi@users.noreply.github.com>
2023-07-25 03:12:13 +0900

    [Silver II] Title: 로또, Time: 56 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/6603. 로또/로또.py
+++ 백준/Silver/6603. 로또/로또.py
@@ -1,14 +1,6 @@
 import sys
+import itertools
 input = sys.stdin.readline
-
-def combination(depth, index):
-    if(depth == 6):
-        print(*com_num)
-        return
-    for i in range(index + 1, n):
-        com_num.append(num[i])
-        combination(depth + 1, i)
-        del com_num[com_num.index(num[i])]
 
 while True:      
     line = input().split()
@@ -18,7 +10,9 @@ while True:
 
     num = [int(line[i]) for i in range(1,n+1)]
     num.sort()
-    com_num = []
-
-    combination(0,-1)
+    
+    combination = list(itertools.combinations(num, 6))
+    for i in range(len(combination)):
+        print(*combination[i])
     print()
+    
