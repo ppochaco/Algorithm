1700b410fb3b549930904ff5afd932b4dffa85f9
A_jin <77830408+ppocchi@users.noreply.github.com>
2023-07-26 13:00:48 +0900

    [Gold IV] Title: 알파벳, Time: 1404 ms, Memory: 52756 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.py
+++ 백준/Gold/1987. 알파벳/알파벳.py
@@ -1,28 +1,26 @@
 import sys
-input = sys.stdin.readline
 
-from collections import deque
 
-r, c = map(int, input().split())
-graph = [list(input().rstrip()) for _ in range(r)]
-queue = deque([(0,0,graph[0][0])])
-dx = [0, 1, -1, 0]
-dy = [1, 0, 0, -1]
-answer = 0
+def bfs():
+    global cnt
+    queue = set([(0, 0, graph[0][0])])
+    while queue:
+        x, y, z = queue.pop()
+        cnt = max(cnt, len(z))
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx < r and 0 <= ny < c and graph[nx][ny] not in z:
+                queue.add((nx, ny, graph[nx][ny] + z))
 
-while queue:
-    x, y, s = queue.pop()
-    answer = max(answer, len(s))
 
-    for i in range(4):
-        cur_x = x + dx[i]
-        cur_y = y + dy[i]
+r, c = map(int, sys.stdin.readline().split())
 
-        if cur_x < 0 or cur_x >= r:
-            continue
-        if cur_y < 0 or cur_y >= c:
-            continue
+graph = [list(map(str, sys.stdin.readline().strip())) for _ in range(r)]
 
-        if graph[cur_x][cur_y] not in s:
-            queue.append((cur_x, cur_y, s + graph[cur_x][cur_y]))
-print(answer)
+dx = [1, -1, 0, 0]
+dy = [0, 0, 1, -1]
+cnt = 1
+
+bfs()
+print(cnt)
