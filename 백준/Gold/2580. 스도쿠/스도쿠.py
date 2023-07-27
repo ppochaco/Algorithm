121cc070d78cb31dc03c43c1115f71c09035579b
A_jin <77830408+ppocchi@users.noreply.github.com>
2023-07-27 17:27:41 +0900

    [Gold IV] Title: 스도쿠, Time: 608 ms, Memory: 118992 KB -BaekjoonHub

--- 백준/Gold/2580. 스도쿠/스도쿠.py
+++ 백준/Gold/2580. 스도쿠/스도쿠.py
@@ -2,41 +2,32 @@ import sys
 input = sys.stdin.readline
 
 graph = [list(map(int, input().split())) for _ in range(9)]
+row = [[0]*10 for _ in range(9)]
+col = [[0]*10 for _ in range(9)]
+square = [[0]*10 for _ in range(9)]
 zero = []
-
-def is_possible(r, c, num):
-    if num in graph[r]:
-        return False
-
-    for i in range(9):
-        if graph[i][c] == num:
-            return False
-    
-    s_i = (r//3) * 3
-    s_j = (c//3) * 3
-    for i in range(3):
-        for j in range(3):
-            if num == graph[s_i + i][s_j + j]:
-                return False
-
-    return True
 
 def sudoku(depth):
     if depth == len(zero):
         return True
     i, j = zero[depth]
     for k in range(1, 10):
-        if is_possible(i, j, k):
+        if not(row[i][k] + col[j][k] + square[(i//3)*3 + (j//3)][k]):
             graph[i][j] = k
+            row[i][k] = col[j][k] = square[(i//3)*3 + (j//3)][k] = 1
             flag = sudoku(depth + 1)
             if flag:
                 return True
+            row[i][k] = col[j][k] = square[(i//3)*3 + (j//3)][k] = 0
             graph[i][j] = 0
     return False
 
 for i in range(9):
     for j in range(9):
-        if not graph[i][j]:
+        if graph[i][j]:
+            k = graph[i][j]
+            row[i][k] = col[j][k] = square[(i//3)*3 + (j//3)][k] = 1
+        else:
             zero.append((i,j))
 
 sudoku(0)
