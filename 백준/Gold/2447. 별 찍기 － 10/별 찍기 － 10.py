f267441f89897cec77e4b5eff1c645fd3593fe85
A_jin <77830408+ppocchi@users.noreply.github.com>
2023-07-28 18:03:55 +0900

    [Gold V] Title: 별 찍기 - 10, Time: 56 ms, Memory: 40604 KB -BaekjoonHub

---
+++ 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline
+
+def print_star(n):
+    if n == 3:
+        return ["***", "* *", "***"]
+
+    sqaure = print_star(n//3)
+    star = []
+    for i in sqaure:
+        star.append(i*3)
+    for i in sqaure:
+        star.append(i + ' ' * (n//3) + i)
+    for i in sqaure:
+        star.append(i*3)
+    return star
+
+n = int(input())
+
+print('\n'.join(print_star(n)))
