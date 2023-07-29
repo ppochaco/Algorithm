313b965fd2a472e99ee50244c4e8866b552e1198
A_jin <77830408+ppocchi@users.noreply.github.com>
2023-07-29 23:02:37 +0900

    [Gold III] Title: 내리막 길, Time: 136 ms, Memory: 42788 KB -BaekjoonHub

---
+++ 백준/Gold/1520. 내리막 길/내리막 길.py
@@ -0,0 +1,32 @@
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(10**6)
+
+n, m = map(int, input().split())
+graph = [list(map(int,input().split())) for _ in range(n)]
+dp = [[-1]*m for _ in range(n)]
+dx = [1, 0, 0,-1]
+dy = [0, 1, -1,0]
+
+def go_down(i,j):
+    if i == n-1 and j == m-1:
+        return 1
+    
+    if dp[i][j] == -1:
+        dp[i][j] = 0
+
+        for k in range(4):
+            nx = i + dx[k]
+            ny = j + dy[k]
+            
+            if nx < 0 or n <= nx:
+                continue
+            if ny < 0 or m <= ny:
+                continue
+
+            if graph[i][j] > graph[nx][ny]:
+                dp[i][j] += go_down(nx,ny)
+        
+    return dp[i][j]
+
+print(go_down(0,0))
