adb0e5aded745add035f4aceeb0daf989bde426d
A_jin <77830408+ppocchi@users.noreply.github.com>
2023-07-31 19:26:27 +0900

    [Silver IV] Title: 치킨 TOP N, Time: 1128 ms, Memory: 147496 KB -BaekjoonHub

--- 백준/Silver/11582. 치킨 TOP N/치킨 TOP N.py
+++ 백준/Silver/11582. 치킨 TOP N/치킨 TOP N.py
@@ -5,19 +5,8 @@ n = int(input())
 chicken = list(map(int, input().split()))
 k = int(input())
 
-def sort_chicken(nums, depth):
-    if len(nums) == 1:
-        return nums
-    
-    m = len(nums) // 2
-    left = nums[:m]
-    right = nums[m:]
-
-    if k != 2**depth:
-        return sort_chicken(left, depth+1) + sort_chicken(right, depth+1)
-    else:
-        c = left + right
-        c.sort()
-        return c
-
-print(*sort_chicken(chicken, 0))
+m = n // k
+s = 0
+while s < n:
+    print(*sorted(chicken[s:s+m]), end=' ')
+    s += m
