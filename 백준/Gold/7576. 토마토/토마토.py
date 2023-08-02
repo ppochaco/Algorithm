507dd9eb14067e2724da2b2e30d01ef79e423216
A_jin <77830408+ppocchi@users.noreply.github.com>
2023-08-02 14:22:24 +0900

    [Gold V] Title: 토마토, Time: 1228 ms, Memory: 98520 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.py
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -7,29 +7,32 @@ graph = [list(map(int, input().split())) for _ in range(n)]
 dx = [1, 0, -1, 0]
 dy = [0, 1, 0, -1]
 queue = deque()
+answer = 0
 
 def bfs():
+    global answer
     while queue:
-        x, y = queue.popleft()
-
-        for i in range(4):
-            nx, ny = x + dx[i], y + dy[i]
-            if nx < 0 or nx >= n or ny < 0 or ny >= m:
-                continue
-            if graph[nx][ny] == 0:
-                graph[nx][ny] = graph[x][y] + 1
-                queue.append((nx,ny))
+        queue_size = len(queue)
+        for _ in range(queue_size):
+            x, y = queue.popleft()
+            for i in range(4):
+                nx, ny = x + dx[i], y + dy[i]
+                if nx < 0 or nx >= n or ny < 0 or ny >= m:
+                    continue
+                if graph[nx][ny] == 0:
+                    graph[nx][ny] = 1
+                    queue.append((nx,ny))
+        answer += 1
 
 for i in range(n):
     for j in range(m):
         if graph[i][j] == 1:
             queue.append((i,j))
+
 bfs()
 
-answer = 0
 for i in graph:
     if 0 in i:
         answer = 0
         break
-    answer = max(max(i), answer)
 print(answer - 1)
