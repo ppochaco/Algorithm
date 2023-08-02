948888db36d43f0f8a63c4e75497e27990b83d81
A_jin <77830408+ppocchi@users.noreply.github.com>
2023-08-02 10:52:34 +0900

    [Gold V] Title: 토마토, Time: 1544 ms, Memory: 98588 KB -BaekjoonHub

---
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -0,0 +1,42 @@
+import sys
+input = sys.stdin.readline
+from collections import deque
+
+m, n = map(int, input().split())
+graph = [list(map(int, input().split())) for _ in range(n)]
+dx = [1, 0, -1, 0]
+dy = [0, 1, 0, -1]
+queue = deque()
+
+def bfs():
+    while queue:
+        x, y = queue.popleft()
+
+        for i in range(4):
+            nx, ny = x + dx[i], y + dy[i]
+            if nx < 0 or nx >= n or ny < 0 or ny >= m:
+                continue
+            if graph[nx][ny] == 0:
+                graph[nx][ny] = graph[x][y] + 1
+                queue.append((nx,ny))
+
+
+for i in range(n):
+    for j in range(m):
+        if graph[i][j] == 1:
+            queue.append((i,j))
+bfs()
+
+answer = 0
+check = False
+for i in range(n):
+    for j in range(m):
+        if graph[i][j] == 0:
+            answer = 0
+            check = True
+            break
+        answer = max(graph[i][j], answer)
+    if check:
+        break
+
+print(answer - 1)
