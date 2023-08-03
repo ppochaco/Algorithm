adbfce4b46833e86cacaa39feb0aad748070c649
A_jin <77830408+ppocchi@users.noreply.github.com>
2023-08-03 11:27:45 +0900

    [Gold IV] Title: 연구소, Time: 2848 ms, Memory: 34668 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.py
+++ 백준/Gold/14502. 연구소/연구소.py
@@ -1,6 +1,7 @@
 import sys, itertools, copy
 input = sys.stdin.readline
 from collections import deque
+from itertools import combinations
 
 n, m = map(int, input().split())
 graph = [list(map(int, input().split())) for _ in range(n)]
@@ -10,43 +11,39 @@ dy = [0, 1, 0, -1]
 answer = 0
 
 def make_walls():
-    three_walls = list(itertools.combinations(blank_index,3))
+    three_walls = list(combinations(blank_index,3))
 
     for wall in three_walls:
+        virus_graph = copy.deepcopy(graph)
         for i in range(3):
             x, y = wall[i]
-            graph[x][y] = 1
+            virus_graph[x][y] = 1
+        spread_virus(virus_graph)
 
-        spread_virus()
 
-        for i in range(3):
-            x, y = wall[i]
-            graph[x][y] = 0
-
-def spread_virus():
+def spread_virus(graph):
     global answer
-    virus_graph = copy.deepcopy(graph)
+    
     queue = deque([])
     for i in range(n):
         for j in range(m):
-            if virus_graph[i][j] == 2:
+            if graph[i][j] == 2:
                 queue.append((i,j))
     
     while queue:
         x, y = queue.popleft()
         for i in range(4):
             nx, ny = x + dx[i], y + dy[i]
-            if 0<=nx<n and 0<=ny<m and virus_graph[nx][ny] == 0:
-                virus_graph[nx][ny] = 2
+            if 0<=nx<n and 0<=ny<m and graph[nx][ny] == 0:
+                graph[nx][ny] = 2
                 queue.append((nx,ny))
     
     blank_cnt = 0
     for i in range(n):
         for j in range(m):
-            if virus_graph[i][j] == 0:
+            if graph[i][j] == 0:
                 blank_cnt += 1
     answer = max(blank_cnt, answer)
-
 
 for i in range(n):
     for j in range(m):
