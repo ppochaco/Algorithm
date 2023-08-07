5e8569bd5dadaaee9025678a62394000f9a600ae
A_jin <77830408+ppocchi@users.noreply.github.com>
2023-08-07 18:26:46 +0900

    [Gold I] Title: 구슬 탈출 2, Time: 68 ms, Memory: 34420 KB -BaekjoonHub

--- 백준/Gold/13460. 구슬 탈출 2/구슬 탈출 2.py
+++ 백준/Gold/13460. 구슬 탈출 2/구슬 탈출 2.py
@@ -4,6 +4,7 @@ from collections import deque
 
 n, m = map(int, input().split())
 board = [list(input().rstrip()) for _ in range(n)]
+visited = [[[[-1]*m for _ in range(n)] for _ in range(m)] for _ in range(n)]
 dx = [1, 0, -1, 0]
 dy = [0, 1, 0, -1]
 
@@ -18,20 +19,21 @@ def move_to_end(i, x, y):
             return nx, ny
         x, y = nx, ny
 
-def bfs(red, blue):
-    queue = deque([(red, blue)])
-    visited = [[[[-1]*m for _ in range(n)] for _ in range(m)] for _ in range(n)]
-    visited[red[0]][red[1]][blue[0]][blue[1]] = 0
+def bfs(rx, ry, bx, by):
+    queue = deque([((rx, ry), (bx, by))])
+    visited[rx][ry][bx][by] = 0
     while queue:
         (rx, ry), (bx, by) = queue.popleft()
+        # 현재 깊이가 10 이상이면 -1 리턴
         if visited[rx][ry][bx][by] > 10:
             return -1
+        # 빨간 공이 구멍에 있다면 현재 깊이 리턴
         if board[rx][ry] == 'O':
             return visited[rx][ry][bx][by]
+        
         for i in range(4):
             nrx, nry = move_to_end(i, rx, ry)
-            nbx, nby = move_to_end(i, bx, by)
-            
+            nbx, nby = move_to_end(i, bx, by)   
             # 파란 공이 구멍에 빠지면 불가능
             if board[nbx][nby] == 'O':
                 continue
@@ -39,7 +41,7 @@ def bfs(red, blue):
             if nrx == nbx and nry == nby:
                 r_size = rx*dx[i] + ry*dy[i]
                 b_size = bx*dx[i] + by*dy[i]
-                # 빨간 공은 그대로, 파란 공은 한 칸 덜 가기
+                # 이전 거리가 큰 공은 그대로, 작은 공은 한 칸 덜 가기
                 if r_size > b_size:
                     nbx, nby = nbx - dx[i], nby - dy[i]
                 else:
@@ -51,12 +53,11 @@ def bfs(red, blue):
             queue.append(((nrx, nry), (nbx, nby)))
     return -1
 
-red = []
-blue = []
 for i in range(n):
     for j in range(m):
         if board[i][j] == 'R':
-            red = (i, j) 
+            rx, ry = (i, j) 
         elif board[i][j] == 'B':
-            blue = (i, j)
-print(bfs(red, blue))
+            bx, by = (i, j)
+
+print(bfs(rx, ry, bx, by))
