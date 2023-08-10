68374a13014fe6f116dd4e794ac22923a7888d5f
A_jin <77830408+ppocchi@users.noreply.github.com>
2023-08-10 17:11:16 +0900

    [Gold II] Title: 2048 (Easy), Time: 380 ms, Memory: 34284 KB -BaekjoonHub

--- 백준/Gold/12100. 2048 （Easy）/2048 （Easy）.py
+++ 백준/Gold/12100. 2048 （Easy）/2048 （Easy）.py
@@ -4,9 +4,7 @@ from collections import deque
 from itertools import product
 
 n = int(input())
-origin = [list(map(int, input().split())) for _ in range(n)]
-direction = ['U', 'L', 'D', 'R']
-dlist = list(product(direction, repeat=5))
+board = [list(map(int, input().split())) for _ in range(n)]
 queue = deque()
 
 def merge(x, y, moves):
@@ -22,41 +20,47 @@ def merge(x, y, moves):
             x, y = x + dx, y + dy
             board[x][y] = num
 
+def move(cur_d):
+    if cur_d == 0:
+        for j in range(n):
+            for i in range(n-1, -1, -1):
+                if board[i][j]:
+                    queue.append(board[i][j])
+                    board[i][j] = 0
+            merge(n-1, j, (-1, 0))
+    elif cur_d == 1:
+        for i in range(n):
+            for j in range(n-1, -1, -1):
+                if board[i][j]:
+                    queue.append(board[i][j])
+                    board[i][j] = 0
+            merge(i, n-1, (0, -1))
+    elif cur_d == 2:
+        for j in range(n):
+            for i in range(n):
+                if board[i][j]:
+                    queue.append(board[i][j])
+                    board[i][j] = 0
+            merge(0, j, (1, 0))
+    else:
+        for i in range(n):
+            for j in range(n):
+                if board[i][j]:
+                    queue.append(board[i][j])
+                    board[i][j] = 0
+            merge(i, 0, (0, 1))
+
 max_num = 0
-for d in dlist:
-    board = copy.deepcopy(origin)
-    
-    for i in range(5):
-        cur_d = d[i]
-        if cur_d == 'U':
-            for j in range(n):
-                for i in range(n-1, -1, -1):
-                    if board[i][j]:
-                        queue.append(board[i][j])
-                        board[i][j] = 0
-                merge(n-1, j, (-1, 0))
-        elif cur_d == 'L':
-            for i in range(n):
-                for j in range(n-1, -1, -1):
-                    if board[i][j]:
-                        queue.append(board[i][j])
-                        board[i][j] = 0
-                merge(i, n-1, (0, -1))
-        elif cur_d == 'D':
-            for j in range(n):
-                for i in range(n):
-                    if board[i][j]:
-                        queue.append(board[i][j])
-                        board[i][j] = 0
-                merge(0, j, (1, 0))
-        else:
-            for i in range(n):
-                for j in range(n):
-                    if board[i][j]:
-                        queue.append(board[i][j])
-                        board[i][j] = 0
-                merge(i, 0, (0, 1))
-
-    max_num = max(max_num, max(map(max, board)))
-
+def dfs(depth):
+    global board, max_num
+    if depth == 5:
+        max_num = max(max_num, max(map(max, board)))
+        return
+    temp = copy.deepcopy(board)
+    for i in range(4):
+        board = copy.deepcopy(temp)
+        move(i)
+        dfs(depth + 1)
+        
+dfs(0)
 print(max_num)
