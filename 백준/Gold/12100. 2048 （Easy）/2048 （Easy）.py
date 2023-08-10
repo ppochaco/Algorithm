5e034d094a2ace2d08952762b657ab650acae1d0
A_jin <77830408+ppocchi@users.noreply.github.com>
2023-08-10 16:30:46 +0900

    [Gold II] Title: 2048 (Easy), Time: 840 ms, Memory: 34268 KB -BaekjoonHub

--- 백준/Gold/12100. 2048 （Easy）/2048 （Easy）.py
+++ 백준/Gold/12100. 2048 （Easy）/2048 （Easy）.py
@@ -1,94 +1,61 @@
 import sys, copy
 input = sys.stdin.readline
+from collections import deque
 from itertools import product
 
 n = int(input())
 origin = [list(map(int, input().split())) for _ in range(n)]
 direction = ['U', 'L', 'D', 'R']
 dlist = list(product(direction, repeat=5))
+queue = deque()
 
-def move_up():
-    for c in range(n):
-        for i in range(1,n):
-            for r in range(i-1, -1, -1):
-                if merged[r][c]:
-                    break
-                if board[r][c] == 0:
-                    board[r][c] = board[r+1][c]
-                    board[r+1][c] = 0
-                    continue
+def merge(x, y, moves):
+    dx, dy = moves
+    while queue:
+        num = queue.popleft()
+        if board[x][y] == 0:
+            board[x][y] = num
+        elif board[x][y] == num:
+            board[x][y] *= 2
+            x, y = x + dx, y + dy
+        else:
+            x, y = x + dx, y + dy
+            board[x][y] = num
 
-                if board[r][c] == board[r+1][c]:
-                    merged[r][c] = True
-                    board[r][c] *= 2
-                    board[r+1][c] = 0
-                break
-
-def move_left():
-    for r in range(n):
-        for i in range(1, n):
-            for c in range(i-1, -1, -1):
-                if merged[r][c]:
-                    break
-                if board[r][c] == 0:
-                    board[r][c] = board[r][c+1]
-                    board[r][c+1] = 0
-                    continue
-
-                if board[r][c] == board[r][c+1]:
-                    merged[r][c] = True
-                    board[r][c] *= 2
-                    board[r][c+1] = 0
-                break
-
-def move_down():
-    for c in range(n):
-        for i in range(n-2, -1, -1):
-            for r in range(i+1, n):
-                if merged[r][c]:
-                    break
-                if board[r][c] == 0:
-                    board[r][c] = board[r-1][c]
-                    board[r-1][c] = 0
-                    continue
-
-                if board[r][c] == board[r-1][c]:
-                    merged[r][c] = True
-                    board[r][c] *= 2
-                    board[r-1][c] = 0
-                break
-
-def move_right():
-    for r in range(n):
-        for i in range(n-2, -1, -1):
-            for c in range(i+1, n):
-                if merged[r][c]:
-                    break
-                if board[r][c] == 0:
-                    board[r][c] = board[r][c-1]
-                    board[r][c-1] = 0
-                    continue
-
-                if board[r][c] == board[r][c-1]:
-                    merged[r][c] = True
-                    board[r][c] *= 2
-                    board[r][c-1] = 0
-                break
 max_num = 0
 for d in dlist:
     board = copy.deepcopy(origin)
     
     for i in range(5):
-        merged = [[False]*n for _ in range(n)]
         cur_d = d[i]
         if cur_d == 'U':
-            move_up()
+            for j in range(n):
+                for i in range(n-1, -1, -1):
+                    if board[i][j]:
+                        queue.append(board[i][j])
+                        board[i][j] = 0
+                merge(n-1, j, (-1, 0))
         elif cur_d == 'L':
-            move_left()
+            for i in range(n):
+                for j in range(n-1, -1, -1):
+                    if board[i][j]:
+                        queue.append(board[i][j])
+                        board[i][j] = 0
+                merge(i, n-1, (0, -1))
         elif cur_d == 'D':
-            move_down()
+            for j in range(n):
+                for i in range(n):
+                    if board[i][j]:
+                        queue.append(board[i][j])
+                        board[i][j] = 0
+                merge(0, j, (1, 0))
         else:
-            move_right()
+            for i in range(n):
+                for j in range(n):
+                    if board[i][j]:
+                        queue.append(board[i][j])
+                        board[i][j] = 0
+                merge(i, 0, (0, 1))
 
     max_num = max(max_num, max(map(max, board)))
 
