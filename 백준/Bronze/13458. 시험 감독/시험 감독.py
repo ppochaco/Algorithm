36f051a4075a23296487b43386fce37e8b12a62b
A_jin <77830408+ppocchi@users.noreply.github.com>
2023-08-11 00:29:23 +0900

    [Bronze II] Title: 시험 감독, Time: 772 ms, Memory: 143400 KB -BaekjoonHub

---
+++ 백준/Bronze/13458. 시험 감독/시험 감독.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+test = list(map(int, input().split()))
+b, c = map(int, input().split())
+cnt = 0
+
+for i in range(n):
+    test[i] -= b
+    cnt += 1
+    if(test[i] > 0):
+        cnt += test[i] // c
+        if test[i] % c != 0:
+            cnt += 1
+
+print(cnt)
