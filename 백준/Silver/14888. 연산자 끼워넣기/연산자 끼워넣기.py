356f6705f30825cacbebe11221af58fd105e459c
A_jin <77830408+ppocchi@users.noreply.github.com>
2023-08-12 15:48:55 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 60 ms, Memory: 31388 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
@@ -3,39 +3,26 @@ input = sys.stdin.readline
 
 n = int(input())
 num = list(map(int, input().split()))
-operation = list(map(int, input().split()))
-op_list = set()
+add, sub, mul, div = map(int, input().split())
 
-def make_op_list(index, op_str):
-    if index == n - 1:
-        op_list.add(op_str)
+def make_formula(index, result, add, sub, mul, div):
+    global max_result, min_result
+    if index == n:
+        max_result = max(max_result, result)
+        min_result = min(min_result, result)
         return
-    for i in range(4):
-        if operation[i] != 0:
-            operation[i] -= 1
-            make_op_list(index + 1, op_str + str(i))
-            operation[i] += 1
-        
-make_op_list(0, '')
+    
+    if add != 0:
+        make_formula(index + 1, result + num[index], add - 1, sub, mul, div)
+    if sub != 0:
+        make_formula(index + 1, result - num[index], add, sub - 1, mul, div)
+    if mul != 0:
+        make_formula(index + 1, result * num[index], add, sub, mul - 1, div)
+    if div != 0:
+        make_formula(index + 1, int(result / num[index]), add, sub, mul, div - 1)
 
-max_ex = -(sys.maxsize - 1)
-min_ex = sys.maxsize
-for op in op_list:
-    expression = num[0]
-    for i in range(n-1):
-        if op[i] == '0':
-            expression += num[i+1]
-        elif op[i] == '1':
-            expression -= num[i+1]
-        elif op[i] == '2':
-            expression *= num[i+1]
-        else:
-            if expression < 0:
-                expression = -(-expression // num[i+1])    
-            else:
-                expression //= num[i+1]
-    max_ex = max(expression, max_ex)
-    min_ex = min(expression, min_ex)
-
-print(max_ex)
-print(min_ex)
+max_result = -(sys.maxsize-1)
+min_result = sys.maxsize
+make_formula(1, num[0], add, sub, mul, div)
+print(max_result)
+print(min_result)
