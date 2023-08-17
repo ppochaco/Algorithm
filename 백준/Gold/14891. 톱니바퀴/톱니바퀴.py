7a1a23556d2336ad06b9462f38eb86c7bad9b82e
A_jin <77830408+ppocchi@users.noreply.github.com>
2023-08-17 13:42:56 +0900

    [Gold V] Title: 톱니바퀴, Time: 40 ms, Memory: 31388 KB -BaekjoonHub

--- 백준/Gold/14891. 톱니바퀴/톱니바퀴.py
+++ 백준/Gold/14891. 톱니바퀴/톱니바퀴.py
@@ -4,41 +4,32 @@ input = sys.stdin.readline
 gears = [list(map(int, input().rstrip())) for _ in range(4)]
 k = int(input())
 
+def select_direction(n):
+    if n+1 < 4 and visited[n+1] == 0 and gears[n+1][6] != gears[n][2]:
+        visited[n+1] = -visited[n]
+        select_direction(n+1)
+    if n-1 >= 0 and visited[n-1] == 0 and gears[n-1][2] != gears[n][6]:
+        visited[n-1] = -visited[n]
+        select_direction(n-1)
+
 def rotate_gear(gear, d):
     if d == 1:
-        rotate = [gear[7]]
-        rotate += gear[:-1]
-        gear = rotate
+        gear = [gear[7]] + gear[:-1]
     if d == -1:
-        rotate = gear[1:]
-        rotate.append(gear[0])
-        gear = rotate
+        gear = gear[1:] + [gear[0]]
     return gear
 
 for _ in range(k):
     n, d = map(int, input().split())
-    n -= 1
-    direction = [0]*4
-    direction[n] = d
-    for i in range(1, 4):
-        if n + i < 4:
-            if gears[n+i][6] != gears[n+i-1][2]:
-                direction[n+i] = -direction[n+i-1]
-            else:
-                break
-    for i in range(1, 4):
-        if n - i >= 0:
-            if gears[n-i+1][6] != gears[n-i][2]:
-                direction[n-i] = -direction[n-i+1]
-            else:
-                break
-
+    visited = [0] * 4
+    visited[n-1] = d
+    
+    select_direction(n-1)
     for i in range(4):
-        gears[i] = rotate_gear(gears[i], direction[i])
-   
-answer = 0     
+        gears[i] = rotate_gear(gears[i], visited[i])
+
+answer = 0
 for i in range(4):
     if gears[i][0] == 1:
-        answer += pow(2, i)
-
+        answer += 2**i
 print(answer)
