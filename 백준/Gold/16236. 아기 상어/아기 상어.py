1aca7a05125147320ba6b42f20f31d2a28673412
A_jin <77830408+ppocchi@users.noreply.github.com>
2023-08-19 17:03:45 +0900

    [Gold III] Title: 아기 상어, Time: 52 ms, Memory: 33324 KB -BaekjoonHub

--- 백준/Gold/16236. 아기 상어/아기 상어.py
+++ 백준/Gold/16236. 아기 상어/아기 상어.py
@@ -3,49 +3,45 @@ input = sys.stdin.readline
 from heapq import heappush, heappop
 
 n = int(input())
-graph = [list(map(int, input().split())) for _ in range(n)]
-start = []
-dx = [-1, 0, 1, 0]
-dy = [0, -1, 0, 1]
+space = [list(map(int, input().split())) for _ in range(n)]
 size = 2
 eat = 0
+start = []
+dx = [0, 1, 0, -1]
+dy = [-1, 0, 1, 0]
 
 def bfs(x, y):
     global size, eat
-    heap = []
-    heappush(heap, (0,x,y))
-    graph[x][y] = 0
     visited = [[0]*n for _ in range(n)]
+    heap = []
+    space[x][y] = 0
+    heappush(heap, (0, x, y))
     while heap:
         depth, x, y = heappop(heap)
-        # 먹기 가능
-        if graph[x][y] != 0 and graph[x][y] < size:
+        if space[x][y] != 0 and space[x][y] < size:
             eat += 1
-            graph[x][y] = 0
+            space[x][y] = 0
             start.append((x,y))
-            # 증가 가능
             if eat == size:
                 eat = 0
                 size += 1
             return visited[x][y]
         for i in range(4):
             nx, ny = x + dx[i], y + dy[i]
-            if not(0<=nx<n) or not(0<=ny<n):
+            if 0 > nx or 0 > ny or n <= nx or n <= ny:
                 continue
-            if visited[nx][ny] or graph[nx][ny] > size:
+            if visited[nx][ny] or space[nx][ny] > size:
                 continue
-            # 이동 가능
+            heappush(heap, (depth+1, nx, ny))
             visited[nx][ny] = depth + 1
-            heappush(heap, (visited[nx][ny], nx, ny))
     return 0
 
 for i in range(n):
     for j in range(n):
-        if graph[i][j] == 9:
+        if space[i][j] == 9:
             start.append((i,j))
 
 time = 0
-for start_index in start:
-    x, y = start_index
-    time += bfs(x,y)
+for i,j in start:
+    time += bfs(i,j)
 print(time)
