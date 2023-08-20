f515396f1968eb29fe328ded9123a535bd0dd57c
A_jin <77830408+ppocchi@users.noreply.github.com>
2023-08-20 21:47:03 +0900

    [Gold IV] Title: 이차원 배열과 연산, Time: 180 ms, Memory: 116188 KB -BaekjoonHub

--- 백준/Gold/17140. 이차원 배열과 연산/이차원 배열과 연산.py
+++ 백준/Gold/17140. 이차원 배열과 연산/이차원 배열과 연산.py
@@ -4,6 +4,41 @@ input = sys.stdin.readline
 r, c, k = map(int, input().split())
 A = [list(map(int, input().split())) for _ in range(3)]
 
+def sort_array(R, C):
+    size = 0
+    for i in range(R):
+        dic = {}
+        for j in range(C):
+            if A[i][j] in dic:
+                dic[A[i][j]] += 1
+            else:
+                dic[A[i][j]] = 1
+        
+        # 0 제거
+        if 0 in dic:
+            del dic[0]
+        # 등장횟수, 수 순서로 정렬
+        dic = sorted(dic.items(), key=lambda x:(x[1],x[0]))
+        
+        A[i] = []
+        for num, times in dic:
+            A[i].append(num)
+            A[i].append(times)
+        size = max(len(dic)*2, size)
+
+    # 0 으로 남은 거 채우기
+    for line in A:
+        remain = 0
+        if len(line) < size:
+            remain = size - len(line)
+        if size < 3:
+            remain = 3 - len(line)
+        for i in range(remain):
+            line.append(0)
+
+    # 가로 크기
+    return size
+
 t = 0
 R, C = 3, 3
 while True:
@@ -15,43 +50,11 @@ while True:
         print(t)
         break
 
-    check = False
-    if R < C:
+    if R >= C:
+        C = sort_array(R, C)
+    else:
         A = list(map(list, zip(*A)))
-        R, C = C, R
-        check = True
-
-    size = 0
-    for i in range(R):
-        dic = {}
-        for j in range(C):
-            if A[i][j] in dic:
-                dic[A[i][j]] += 1
-            else:
-                dic[A[i][j]] = 1
-        if 0 in dic:
-            del dic[0]
-        dic = sorted(dic.items(), key=lambda x:(x[1],x[0]))
-        
-        A[i] = []
-        for j in dic:
-            num, times = j
-            A[i].append(num)
-            A[i].append(times)
-        size = max(len(dic)*2, size)
-
-    for line in A:
-        if len(line) < size:
-            remain = size - len(line)
-            for i in range(remain):
-                line.append(0)
-        if size < 3:
-            remain = 3 - len(line)
-            for i in range(remain):
-                line.append(0)
-    C = size
-    if check:
+        R = sort_array(C, R)
         A = list(map(list, zip(*A)))
-        R, C = C, R
 
     t += 1
