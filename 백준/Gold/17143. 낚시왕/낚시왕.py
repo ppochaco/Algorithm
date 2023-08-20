b9b79f2c5f1e6dd3503689f12e6b746d273338ad
A_jin <77830408+ppocchi@users.noreply.github.com>
2023-08-20 18:45:02 +0900

    [Gold I] Title: 낚시왕, Time: 388 ms, Memory: 167440 KB -BaekjoonHub

--- 백준/Gold/17143. 낚시왕/낚시왕.py
+++ 백준/Gold/17143. 낚시왕/낚시왕.py
@@ -9,23 +9,38 @@ for _ in range(m):
 dx = [-1, 1, 0, 0]
 dy = [0, 0, 1, -1]
 
+def next_shark_d(x,y,s,d):
+    if d//2 == 0:
+        cycle = (r-1)*2
+        if d % 2 == 0:
+            s += cycle - x
+        else:
+            s += x
+        s %= cycle
+        if s >= r:
+            return (cycle - s, y, 0)
+        return (s, y, 1)
+    else:
+        cycle = (c-1)*2
+        if d % 2 == 1:
+            s += cycle - y
+        else:
+            s += y
+        s %= cycle
+        if s >= c:
+            return (x, cycle - s, 3)
+        return (x, s, 2)
+    
 def move_sharks():
     temp_sharks = [[[] for _ in range(c)] for _ in range(r)]
     for i in range(r):
         for j in range(c):
             if sharks[i][j]:
-                x, y = i, j
                 s, d, z = sharks[i][j][0]
-                for _ in range(s):
-                    x, y = x + dx[d], y + dy[d]
-                    if ((0 > x or x >= r) and (d//2 == 0)) or ((0 > y or y >= c) and (d//2 == 1)):
-                        if d % 2 == 1:
-                            d -= 1
-                        else:
-                            d += 1
-                        x += dx[d]*2
-                        y += dy[d]*2
+                # 상어 이동 위치
+                x, y, d = next_shark_d(i,j,s,d)
                 temp_sharks[x][y].append((s,d,z))
+    
     for i in range(r):
         for j in range(c):
             sharks[i][j] = temp_sharks[i][j]
@@ -34,6 +49,7 @@ def eat_sharks():
     for i in range(r):
         for j in range(c):
             if len(sharks[i][j]) > 1:
+                # 크기가 가장 큰 상어만 살아남기
                 sharks[i][j].sort(key=lambda x:x[2], reverse=True)
                 sharks[i][j] = [(sharks[i][j][0])]
 
