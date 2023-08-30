47d00134370e9f44b079995b5c67b9fe1221a799
A_jin <77830408+ppocchi@users.noreply.github.com>
2023-08-30 21:11:46 +0900

    [Gold I] Title: 마법사 상어와 블리자드, Time: 112 ms, Memory: 31388 KB -BaekjoonHub

--- 백준/Gold/21611. 마법사 상어와 블리자드/마법사 상어와 블리자드.py
+++ 백준/Gold/21611. 마법사 상어와 블리자드/마법사 상어와 블리자드.py
@@ -1,72 +1,72 @@
-import sys, copy
+import sys
 input = sys.stdin.readline
 
-n, m = map(int, input().split())
+n,m = map(int,input().split())
 board = [list(map(int, input().split())) for _ in range(n)]
 magic = []
+location = {}
 for _ in range(m):
     d, s = map(int, input().split())
-    magic.append((d-1, s))
+    magic.append((d,s))
 
-def destroy_beads(d, s):
-    x, y = n//2, n//2
-    dx = [-1, 1, 0, 0]
-    dy = [0, 0, -1, 1]
-    
-    for _ in range(s):
-        x, y = (x + dx[d]) % n, (y + dy[d]) % n
-        board[x][y] = 0
-
-def move_beads():
+def init():
     x, y = n//2, n//2
     dx = [0, 1, 0, -1]
     dy = [-1, 0, 1, 0]
-    k = 0
-
+    index, depth = 1, 0
     while True:
         for i in range(4):
             if i % 2 == 0:
-                k += 1
-            for _ in range(k):
+                depth += 1
+            
+            for _ in range(depth):
                 x, y = x + dx[i], y + dy[i]
-                if board[x][y] != 0:
-                    beads.append(board[x][y])
+                location[(x,y)] = index
+                beads.append(board[x][y])
+                index += 1
                 if x == 0 and y == 0:
                     return
 
-def explore_beads():
-    global answer, beads
-    while True:
-        if not beads:
-            return
-        cur_num, count = beads[0], 1
-        temp_beads = []
+def destroy(d, s):
+    x, y = n//2, n//2
+    dx = [-1, 1, 0, 0]
+    dy = [0, 0, -1, 1]
 
-        for i in range(1, len(beads)):
-            if cur_num == beads[i]:
-                count += 1
-            else:
-                if count >= 4:
-                    answer += cur_num * count
-                else:
-                    for j in range(i-1, i-count-1, -1):
-                        temp_beads.append(beads[j])
-                cur_num, count = beads[i], 1
-        if count >= 4:
-            answer += cur_num * count
-        else:
-            for j in range(len(beads)-1, len(beads)-count-1, -1):
-                temp_beads.append(beads[j])
+    for _ in range(s):
+        x, y = (x + dx[d]) % n, (y + dy[d]) % n
+        index = location[(x,y)]
+        beads[index] = 0
 
-        if beads == temp_beads:
-            return
-
-        beads = temp_beads
-
-def change_beads():
+def move():
     global beads
-    cur_num, count = beads[0], 1
-    temp_beads = []
+    beads = [v for v in beads if v != 0]
+
+    beads.extend([0]*((n*n-1) - len(beads)))
+    beads = [0] + beads
+
+    
+def explosion():
+    global answer
+    cur_num, count, start = beads[0], 0, 0
+    flag = False
+    for i, bead in enumerate(beads):
+        if cur_num == bead:
+            count += 1
+        else:
+            if count >= 4:
+                for j in range(start, i):
+                    beads[j] = 0
+                answer += cur_num * count
+                flag = True
+
+            cur_num, count, start = bead, 1, i
+
+    return flag
+
+def change():
+    global beads
+    cur_num, count = beads[1], 0
+    temp_beads = [0]
 
     for i in range(1, len(beads)):
         if cur_num == beads[i]:
@@ -74,45 +74,23 @@ def change_beads():
         else:
             temp_beads.extend((count, cur_num))
             cur_num, count = beads[i], 1
-    temp_beads.extend((count, cur_num))
+    if len(temp_beads) > len(beads):
+        temp_beads = temp_beads[:len(beads)]
+    else:
+        temp_beads.extend([0]*(len(beads)-len(temp_beads)))
 
     beads = temp_beads
 
-def beads_to_graph():
-    global board
-    x, y = n//2, n//2
-    dx = [0, 1, 0, -1]
-    dy = [-1, 0, 1, 0]
-    k = 0
-    index = 0
-    temp_board = [[0]*n for _ in range(n)]
-    while True:
-        for i in range(4):
-            if i % 2 == 0:
-                k += 1
-            for _ in range(k):
-                if index == len(beads):
-                    board = copy.deepcopy(temp_board)
-                    return
-                x, y = x + dx[i], y + dy[i]
-                temp_board[x][y] = beads[index]
-                index += 1
-                
-                if x == 0 and y == 0:
-                    board = copy.deepcopy(temp_board)
-                    return
-
 answer = 0
+beads = [0]
+
+init()
+
 for d, s in magic:
-    beads = []
-    destroy_beads(d, s)
-    move_beads()
-    if not beads:
-        break
-    explore_beads()
-    if not beads:
-        break
-    change_beads()
-    beads_to_graph()
+    destroy(d-1, s)
+    move()
+    while explosion():
+        move()
+    change()
 
 print(answer)
