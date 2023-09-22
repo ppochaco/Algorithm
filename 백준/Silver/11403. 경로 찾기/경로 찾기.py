c25a15e686ac1ed0728f222fa1a7c8638dcc81de
A_jin <77830408+ppocchi@users.noreply.github.com>
2023-09-22 18:54:19 +0900

    [Silver I] Title: 경로 찾기, Time: 248 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/11403. 경로 찾기/경로 찾기.py
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.py
@@ -3,26 +3,19 @@ input = sys.stdin.readline
 
 n = int(input())
 graph = [list(map(int, input().split())) for _ in range(n)]
-temp_graph = graph[:]
-
-INF = sys.maxsize
-for i in range(n):
-    for j in range(n):
-        if graph[i][j] == 0:
-            graph[i][j] = INF
 
 def floyd_warshall():
     for k in range(n):
         for i in range(n):
             for j in range(n):
-                graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j])
+                # i에서 k점 지나기 가능하면
+                if graph[i][k] == 1:
+                    # k에서 j 지나기 가능한지 체크
+                    graph[i][j] = max(graph[i][j], graph[k][j])
         
 floyd_warshall()
 
 for i in range(n):
     for j in range(n):
-        if graph[i][j] != INF and graph[i][j] != 0:
-            print(1, end=' ')
-        else:
-            print(0, end=' ')
+        print(graph[i][j], end=' ')
     print()
