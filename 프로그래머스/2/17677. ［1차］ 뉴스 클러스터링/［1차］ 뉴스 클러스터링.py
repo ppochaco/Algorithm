5d811578298bf6cec2b7bd56b72493a879efdc2e
A_jin <77830408+ppocchi@users.noreply.github.com>
2023-11-21 21:01:24 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 0.10 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.py
+++ 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.py
@@ -1,62 +1,53 @@
 import math
-def getOrd(s):
+
+def stringToList(s):
+    # 문자열중에서 영문자만 선택
     newStr = ''
-    
     for i in s:
         if i.isalpha():
             newStr += i.upper()
         else:
             newStr += '_'
             
-    return newStr
+    # 빈 문자열 제외하고 두 문자씩 묶어서 리스트 만들기
+    clist = []
+    for i in range(len(newStr)-1):
+        if newStr[i] != '_' and newStr[i+1] != '_':
+            clist.append(newStr[i] + newStr[i+1])
+            
+    return clist
 
 def solution(str1, str2):
     answer = 0
     
-    str1 = getOrd(str1)
-    str2 = getOrd(str2)
-
-    list1 = []
-    for i in range(len(str1)-1):
-        if not(str1[i] == '_' or str1[i+1] == '_'):
-            list1.append(str1[i] + str1[i+1])
+    list1 = stringToList(str1)
+    list2 = stringToList(str2)
     
-    list2 = []
-    for i in range(len(str2)-1):
-        if not (str2[i] == '_' or str2[i+1] == '_'):
-            list2.append(str2[i] + str2[i+1])
-            
-    print(list1)
-    print(list2)
-    
-    newlist1 = []
-    for c in list1:
-        if '_' not in c:
-            newlist1.append(c)
-            
-    newlist2 = []
-    for c in list2:
-        if '_' not in c:
-            newlist2.append(c)
-    
-    if not len(newlist1 + newlist2):
+    # 모두 빈 리스트면 자카드 유사도 1
+    if not len(list1 + list2):
         return 1 * 65536
     
-    arr = set(newlist1 + newlist2)
+    # 자카드 유사도 구하기
+    arr = set(list1 + list2)
     allarr = 0
     subarr = 0
+    
     for c in arr:
         count1 = 0
         count2 = 0
     
-        if c in newlist1:
-            count1 += newlist1.count(c)
-        if c in newlist2:
-            count2 += newlist2.count(c)
+        if c in list1:
+            count1 += list1.count(c)
+        if c in list2:
+            count2 += list2.count(c)
         
+        # 두 리스트 모두 같은 요소 가지면 적은 개수를 교집합 개수에 더하기
         if count1 != 0 and count2 != 0:
-            subarr += min(count1, count2)      
+            subarr += min(count1, count2) 
+            
+        # 합집합에 두 리스트의 요소의 개수 더하기
         allarr += count1 + count2
-    # print(subarr, allarr)
+        
     answer = math.floor(subarr / (allarr-subarr) * 65536)
+    
     return answer
