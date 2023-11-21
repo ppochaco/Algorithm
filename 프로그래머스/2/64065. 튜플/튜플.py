2c38bee95166819c91e4242768d6e0df60d31aab
A_jin <77830408+ppocchi@users.noreply.github.com>
2023-11-21 16:49:05 +0900

    [level 2] Title: 튜플, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.py
+++ 프로그래머스/2/64065. 튜플/튜플.py
@@ -1,19 +1,13 @@
 def solution(s):
     s = s[2:len(s)-2].split('},{')
+    answer = 0
+    s.sort(key = lambda x : len(x))
     
-    s.sort(key=len)
-    maxtuple = s[len(s)-1].split(',')
-    
-    stuple = []
-    for num in s:
-        stuple.extend(num.split(','))
-    
-    answer = [0] * len(maxtuple)
-    for i, num in enumerate(maxtuple):
-        index = len(maxtuple) - stuple.count(num)
-        answer[index] = int(num)
-    
-    
-    
+    answer = []
+    for nums in s:
+        nums = list(map(int, nums.split(',')))
+        for num in nums:
+            if num not in answer:
+                answer.append(num)
 
     return answer
