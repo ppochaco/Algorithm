81e989511a763f419aca3c6835324f60fc17386f
A_jin <77830408+ppocchi@users.noreply.github.com>
2023-11-23 22:55:03 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.34 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.py
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.py
@@ -11,18 +11,15 @@ def solution(new_id):
     new_id = temp_id
     
     # 3단계 new_id에서 마침표(.)가 2번 이상 연속된 부분을 하나의 마침표(.)로 치환합니다.
-    while True:
-        temp_id = new_id.replace("..", '.')
-        if new_id == temp_id:
-            break
-        new_id = temp_id
+    while '..' in new_id:
+        new_id = new_id.replace("..", '.')
 
     # 4단계 new_id에서 마침표(.)가 처음이나 끝에 위치한다면 제거합니다.
     if new_id[0] == '.':
         new_id = new_id[1:]
     if len(new_id):
-        if new_id[len(new_id)-1] == '.':
-            new_id = new_id[:len(new_id)-1]
+        if new_id[-1] == '.':
+            new_id = new_id[:-1]
     
     # 5단계 new_id가 빈 문자열이라면, new_id에 "a"를 대입합니다.
     if new_id == '':
@@ -32,13 +29,11 @@ def solution(new_id):
     # 만약 제거 후 마침표(.)가 new_id의 끝에 위치한다면 끝에 위치한 마침표(.) 문자를 제거합니다.
     if len(new_id) >= 16:
         new_id = new_id[:15]
-    if new_id[len(new_id)-1] == '.':
-        new_id = new_id[:-1]
+        if new_id[-1] == '.':
+            new_id = new_id[:-1]
     
     # 7단계 new_id의 길이가 2자 이하라면, new_id의 마지막 문자를 new_id의 길이가 3이 될 때까지 반복해서 끝에 붙입니다.
-    if len(new_id) <= 2:
-        c = new_id[-1]
-        while len(new_id) < 3:
-            new_id += c
+    while len(new_id) < 3:
+        new_id += new_id[-1]
             
     return new_id
