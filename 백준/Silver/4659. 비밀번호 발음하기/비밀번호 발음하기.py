68c0d7db70108ddc3d623b9302705ad572b8c072
A_jin <77830408+ppocchi@users.noreply.github.com>
2024-01-11 22:56:52 +0900

    [Silver V] Title: 비밀번호 발음하기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/4659. 비밀번호 발음하기/비밀번호 발음하기.py
+++ 백준/Silver/4659. 비밀번호 발음하기/비밀번호 발음하기.py
@@ -1,44 +1,43 @@
 import sys
 input = sys.stdin.readline
 
-vowel = ['a', 'e', 'i', 'o', 'u']
-alpha = []
-for i in range(26):
-    c = chr(ord('a')+i)
-    alpha.append(c+c)
-alpha.remove('ee')
-alpha.remove('oo')
+vowel = 'aeiou'
+eo = 'eo'
 
 def check_password(word):
-    # 1. 모음 하나 이상
-    todigit = ''
+    isvowel = False
+    stack1 = 0
+    stack2 = 0
+    pre = ''
     for c in word:
         if c in vowel:
-            todigit += '1'
+            stack1 += 1
+            stack2 = 0
+            isvowel = True
         else:
-            todigit += '0'
-    if todigit.find('1') == -1:
-        return False
-
-    # 2. 모음, 자음 연속 2개 이하
-    if todigit.find('111') != -1:
-        return False
-    if todigit.find('000') != -1:
-        return False
-    
-    # 3. 같은 글자 2개 연속 불가(ee, oo 제외)
-    for cc in alpha:
-        if cc in word:
+            stack2 += 1
+            stack1 = 0
+        # 2
+        if stack1 == 3 or stack2 == 3:
             return False
+        # 3
+        if pre == c and (c not in eo):
+            return False
+        
+        pre = c
+
+    # 1
+    if not isvowel:
+        return False
     
     return True
-
+    
 while True:
     password = input().strip()
     if password == 'end':
         break
     
     if check_password(password):
-        print("<"+password+"> is acceptable.")
+        print(f'<{password}> is acceptable.')
     else:
-        print("<"+password+"> is not acceptable.")
+        print(f'<{password}> is not acceptable.')
