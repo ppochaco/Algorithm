979379a59556a46ad33b4422a91f2c7c859f5ade
A_jin <77830408+ppocchi@users.noreply.github.com>
2024-01-26 22:59:52 +0900

    [Silver I] Title: 겹치는 건 싫어, Time: 228 ms, Memory: 53884 KB -BaekjoonHub

--- 백준/Silver/20922. 겹치는 건 싫어/겹치는 건 싫어.py
+++ 백준/Silver/20922. 겹치는 건 싫어/겹치는 건 싫어.py
@@ -1,24 +1,21 @@
 import sys
+from collections import Counter, deque
 input = sys.stdin.readline
 
 n, k = map(int, input().split())
 arr = list(map(int, input().split()))
 
-num = [0]*(max(arr)+1)
-start, end = 0, 0
+count = [0]*(max(arr)+1)
 max_len = 0
-while True:
-    if start == n:
-        break
+end = 0
 
-    if num[arr[start]] >= k:
-        num[arr[end]] -= 1
+for start, num in enumerate(arr):
+    count[num] += 1
+
+    while count[num] > k:
+        count[arr[end]] -= 1
         end += 1
-    else:
-        num[arr[start]] += 1
-        start += 1
-
-    # print(start, end)
-    max_len = max(start-end, max_len)
+    
+    max_len = max(max_len, start-end+1)
 
 print(max_len)
