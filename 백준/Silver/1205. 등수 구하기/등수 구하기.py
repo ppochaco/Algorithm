28f85ae2e0fa332488872eee879c25c11689a92a
A_jin <77830408+ppochaco@users.noreply.github.com>
2024-02-13 23:17:24 +0900

    [Silver IV] Title: 등수 구하기, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1205. 등수 구하기/등수 구하기.py
@@ -0,0 +1,37 @@
+import sys
+input = sys.stdin.readline
+
+def get_rank():
+    global grade
+
+    grade.append(new)
+    grade.sort(reverse=True)
+    if len(grade) > p:
+        grade = grade[:p]
+    
+    ranking = 1
+    for cur in grade:
+        if cur == new:
+            return ranking
+        ranking += 1
+
+    return -1
+
+n, new, p = map(int, input().split())
+if n == 0:
+    print(1)
+else:
+    grade = list(map(int, input().split()))
+    cur_min = grade[-1]
+    if n == p:
+        if cur_min >= new:
+            print(-1)
+        else:
+            # 등수 구하기
+            print(get_rank())
+    else:
+        if cur_min > new:
+            print(n+1)
+        else:
+            # 등수 구하기
+            print(get_rank())
