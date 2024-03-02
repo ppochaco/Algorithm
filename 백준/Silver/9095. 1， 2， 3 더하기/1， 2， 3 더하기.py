ad56812b39be4f5013c9a11c496894d6665d6cce
A_jin <77830408+ppochaco@users.noreply.github.com>
2024-03-02 16:20:54 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
@@ -2,13 +2,10 @@ import sys
 input = sys.stdin.readline
 
 t = int(input())
-method = [1, 2, 4]
-for i in range(3, 11):
-    cur = method[i-1] + method[i-2] + method[i-3]
-    method.append(cur)
-
-# print(method)
 
 for _ in range(t):
     n = int(input())
-    print(method[n-1])
+    method = [0, 1 ,2, 4]
+    for i in range(4, n+1):
+        method.append(sum(method[i-3:i]))
+    print(method[n])
