e9f79f655a9ec2afa40cb3ff0ba33ad86c1ad831
A_jin <77830408+ppochaco@users.noreply.github.com>
2024-04-04 21:23:02 +0900

    [Gold IV] Title: 최단경로, Time: 500 ms, Memory: 164592 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.py
+++ 백준/Gold/1753. 최단경로/최단경로.py
@@ -1,34 +1,36 @@
 import sys
-from heapq import heappop, heappush
+from heapq import heappush, heappop
 input = sys.stdin.readline
 
-n, m = map(int, input().split())
-start = int(input())
-INF = int(1e9)
-distance = [INF] * (n+1)
-graph = [[] for _ in range(n+1)]
-for _ in range(m):
-    a, b, c = map(int, input().split())
-    graph[a].append((b,c))
+v, e = map(int, input().split())
+k = int(input())
 
-def dijkstra(start):
+INF = sys.maxsize
+graph = [[] for _ in range(v+1)]
+for _ in range(e):
+    start, end, weight = map(int, input().split())
+    graph[start].append((end, weight))
+
+def dajikstra(start):
     queue = []
+    visited = [INF] * (v+1)
+
     heappush(queue, (0, start))
-    distance[start] = 0
+    visited[start] = 0
     while queue:
-        dist, node = heappop(queue)
-        if distance[node] != dist:
+        cur_weight, cur_node = heappop(queue)
+        if visited[cur_node] < cur_weight:
             continue
-        for next in graph[node]:
-            cost = distance[node] + next[1]
-            if cost < distance[next[0]]:
-                distance[next[0]] = cost
-                heappush(queue, (cost, next[0]))
+        for next_node, next_weight in graph[cur_node]:
+            if visited[next_node] < cur_weight + next_weight:
+                continue
+            visited[next_node] = cur_weight + next_weight
+            heappush(queue, (visited[next_node], next_node))
 
-dijkstra(start)
+    for i in range(1, v+1):
+        if visited[i] == sys.maxsize:
+            print('INF')
+        else:
+            print(visited[i])
 
-for i in range(1, len(distance)):
-    if distance[i] == INF:
-        print('INF')
-    else:
-        print(distance[i])
+dajikstra(k)
