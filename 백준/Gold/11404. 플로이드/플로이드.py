a733b804a3056e4af1f4b4d9b5b6380e63f722b6
A_jin <77830408+ppochaco@users.noreply.github.com>
2024-04-25 17:25:51 +0900

    [Gold IV] Title: 플로이드, Time: 408 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.py
+++ 백준/Gold/11404. 플로이드/플로이드.py
@@ -1,32 +1,30 @@
 import sys
 input = sys.stdin.readline
 
-INF = int(1e9)
 n = int(input())
 m = int(input())
-graph = [[INF]*(n+1) for _ in range(n+1)]
 
-for i in range(1, n+1):
-    for j in range(1, n+1):
-        if i == j:
-            graph[i][j] = 0
-
+INF = sys.maxsize
+graph = list([INF]*(n+1) for _ in range(n+1))
 for _ in range(m):
     a, b, c = map(int, input().split())
     graph[a][b] = min(graph[a][b], c)
+
+for i in range(1, n+1):
+    graph[i][i] = 0
 
 def floyd_warshall():
     for k in range(1, n+1):
         for i in range(1, n+1):
             for j in range(1, n+1):
-               graph[i][j] = min(graph[i][j], graph[i][k]+graph[k][j])
+                graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j])
 
 floyd_warshall()
 
 for i in range(1, n+1):
     for j in range(1, n+1):
         if graph[i][j] == INF:
-            print(0, end=" ")
+            print(0, end=' ')
         else:
-            print(graph[i][j], end=" ")
+            print(graph[i][j], end=' ')
     print()
