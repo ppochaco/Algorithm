6893c218b28638af65b80689a5d5e2af1b93f1a0
A_jin <77830408+ppochaco@users.noreply.github.com>
2024-05-01 15:18:22 +0900

    [Silver V] Title: 숫자 카드, Time: 1876 ms, Memory: 112892 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.py
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.py
@@ -7,20 +7,21 @@ m = int(input())
 m_card = list(map(int, input().split()))
 
 n_card.sort()
-
 answer = []
+
 def find_card(card, left, right):
-    if left > right:
-        answer.append(0)
-        return
-    mid = (left + right) // 2
-    if n_card[mid] > card:
-        find_card(card, left, mid - 1)
-    elif n_card[mid] < card:
-        find_card(card, mid + 1, right)
-    else:
-        answer.append(1)
-        return
+    ischecked = 0
+    while left <= right:
+        mid = (left + right) // 2
+        if n_card[mid] < card:
+            left = mid + 1
+        elif n_card[mid] > card:
+            right = mid - 1
+        else:
+            ischecked = 1
+            break
+    answer.append(ischecked)
+
 for i in m_card:
     find_card(i, 0, len(n_card)-1)
 
