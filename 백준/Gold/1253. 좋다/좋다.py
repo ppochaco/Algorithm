9d8bf6fb0437a3d6889aabca2c0fc9a78368047f
A_jin <77830408+ppochaco@users.noreply.github.com>
2024-05-03 21:55:50 +0900

    [Gold IV] Title: 좋다, Time: 380 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/1253. 좋다/좋다.py
+++ 백준/Gold/1253. 좋다/좋다.py
@@ -4,27 +4,29 @@ input = sys.stdin.readline
 n = int(input())
 num = list(map(int, input().split()))
 
-def is_good_num(cur_num):
-    num_list = num[:]
-    num_list.pop(num_list.index(cur_num))
+def is_good_num(cur_num, index):
     left = 0
-    right = len(num_list) - 1
+    right = len(num) - 1
     while left < right:
-        two_num_sum = num_list[left] + num_list[right]
-
+        two_num_sum = num[left] + num[right]
         if two_num_sum < cur_num:
             left += 1
         elif two_num_sum > cur_num:
             right -= 1
         else:
-            return True
+            if left == index:
+                left += 1
+            elif right == index:
+                right -= 1
+            else:
+                return True
     return False
 
 num.sort()
 
 answer = 0
 for i in range(n):
-    if is_good_num(num[i]):
+    if is_good_num(num[i], i):
         answer += 1
 
 print(answer)
