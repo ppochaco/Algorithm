5745ff88b67b891907358f5cd327e40a2d432898
A_jin <77830408+ppochaco@users.noreply.github.com>
2024-05-17 17:27:06 +0900

    [Gold III] Title: 양팔저울, Time: 380 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/2629. 양팔저울/양팔저울.py
@@ -0,0 +1,29 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+weight = list(map(int, input().split()))
+m = int(input())
+marble = list(map(int, input().split()))
+
+max_weight = sum(weight)
+dp = [1] + [0]*max_weight
+
+for cur_weight in weight:
+    cur_dp = dp[:]
+    for cur_num in range(max_weight, 0, -1):
+        cur_idx = abs(cur_num - cur_weight)
+        cur_dp[cur_num] = max(dp[cur_idx], dp[cur_num])
+        if cur_num + cur_weight < max_weight:
+            cur_dp[cur_num] = max(cur_dp[cur_num], dp[cur_num+cur_weight])
+    dp = cur_dp[:]
+
+for cur_marble in marble:
+    if cur_marble > max_weight:
+        print('N', end=' ')
+        continue
+
+    if dp[cur_marble]:
+        print('Y', end=' ')
+    else:
+        print('N', end=' ')
