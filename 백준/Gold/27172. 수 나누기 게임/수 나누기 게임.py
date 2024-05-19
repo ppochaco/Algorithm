02579fdbc14837adcc52ab0a3d2921a60d8b345f
A_jin <77830408+ppochaco@users.noreply.github.com>
2024-05-19 18:22:08 +0900

    [Gold V] Title: 수 나누기 게임, Time: 1564 ms, Memory: 45568 KB -BaekjoonHub

--- 백준/Gold/27172. 수 나누기 게임/수 나누기 게임.py
+++ 백준/Gold/27172. 수 나누기 게임/수 나누기 게임.py
@@ -3,25 +3,15 @@ input = sys.stdin.readline
 
 n = int(input())
 player = list(map(int, input().split()))
-origin_player = player[:]
-player.sort()
 
 max_num = max(player)
-nums = [[] for _ in range(max_num+1)]
+nums = {cur_player: 0 for cur_player in player}
+
 for cur_player in player:
-    cur_num = cur_player*2
-    while cur_num <= max_num:
-        nums[cur_num].append(cur_player)
-        cur_num += cur_player
+    for cur_num in range(cur_player*2, max_num+1, cur_player):
+        if cur_num in nums:
+            nums[cur_player] += 1
+            nums[cur_num] -= 1
 
-score = [0] * (max_num+1)
 for cur_player in player:
-    if not nums[cur_player]:
-        continue
-
-    for win in nums[cur_player]:
-        score[win] += 1
-        score[cur_player] -= 1
-
-for i in origin_player:
-    print(score[i], end=' ')
+    print(nums[cur_player], end=' ')
