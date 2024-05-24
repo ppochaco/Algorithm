9ede927b22cfcf3669d33eb0eb9acb1cd6cdb173
A_jin <77830408+ppochaco@users.noreply.github.com>
2024-05-24 17:25:26 +0900

    [Gold V] Title: 평범한 배낭, Time: 3388 ms, Memory: 34972 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -2,16 +2,11 @@ import sys
 input = sys.stdin.readline
 
 n, k = map(int, input().split())
-stuff = []
+
+dp = [0] * (k+1)
 for _ in range(n):
     w, v = map(int, input().split())
-    stuff.append((w, v))
-stuff.sort(key=lambda x:x[0])
+    for i in range(k, w-1, -1):
+        dp[i] = max(dp[i], dp[i-w] + v)
 
-dp = [0]*(k+1)
-for cur_weight, cur_value in stuff:
-    for weight in range(k, 0, -1):
-        if cur_weight <= weight:
-            dp[weight] = max(dp[weight], dp[weight - cur_weight] + cur_value)
-
-print(max(dp))
+print(dp[k])
