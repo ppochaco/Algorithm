aa137f5381927e3cf30961332a33732feb2b4dea
A_jin <77830408+ppochaco@users.noreply.github.com>
2024-05-26 17:11:20 +0900

    [Platinum IV] Title: 평범한 배낭 2, Time: 4396 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Platinum/12920. 평범한 배낭 2/평범한 배낭 2.py
+++ 백준/Platinum/12920. 평범한 배낭 2/평범한 배낭 2.py
@@ -6,15 +6,13 @@ n, m = map(int, input().split())
 items = []
 for _ in range(n):
     v, c, k = map(int, input().split())
-    i = 0
-    while k > 0:
-        if k < 2**i:
-            items.append((k * v, k * c))
-            break
 
-        items.append((2**i * v, 2**i * c))
-        k -= 2**i
-        i += 1
+    i = 1
+    while k > 0:
+        cnt = min(i, k)
+        items.append((v * cnt, c * cnt))
+        k -= i
+        i *= 2
 
 dp = [0] * (m+1)
 for v, c in items:
