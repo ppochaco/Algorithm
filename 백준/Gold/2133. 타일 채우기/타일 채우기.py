5fe0cf785f8205ec828b497b60051f27122a0576
A_jin <77830408+ppochaco@users.noreply.github.com>
2024-06-05 01:18:15 +0900

    [Gold IV] Title: 타일 채우기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/2133. 타일 채우기/타일 채우기.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+dp = [0] * 31
+dp[0] = 1
+dp[2] = 3
+
+for i in range(4, n+1, 2):
+    dp[i] = dp[i-2] * 3
+    for j in range(4, i+1, 2):
+        dp[i] += dp[i-j] * 2
+print(dp[n])
