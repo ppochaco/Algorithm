144a7e1aba21ba9f5ef08b06246b618def96c591
A_jin <77830408+ppochaco@users.noreply.github.com>
2025-02-27 13:54:58 +0900

    [Gold IV] Title: 괄호 제거, Time: 100 ms, Memory: 11100 KB -BaekjoonHub

--- 백준/Gold/2800. 괄호 제거/괄호 제거.js
+++ 백준/Gold/2800. 괄호 제거/괄호 제거.js
@@ -17,39 +17,27 @@ for (let i = 0; i < input.length; i++) {
   }
 }
 
-const answer = new Set();
-for (let i = 1; i <= parentheses_index.length; i++) {
-  const i_combinations = combinations(parentheses_index, i);
+let answer = new Set();
 
-  for (let j = 0; j < i_combinations.length; j++) {
-    const temp = [...input];
-    for (let k = 0; k < i_combinations[j].length; k++) {
-      const cur = i_combinations[j][k];
-      temp[cur.start] = temp[cur.end] = " ";
-    }
-    answer.add(temp.join("").replace(/ /g, ""));
-  }
-}
+function dfs(depth, arr, index) {
+  if (depth === parentheses_index.length) {
+    if (index === parentheses_index.length) return;
 
-console.log([...answer].sort().join("\n"));
-
-// 조합
-function combinations(arr, r) {
-  const result = [];
-
-  function combination(cur_arr, index) {
-    if (cur_arr.length === r) {
-      result.push([...cur_arr]);
-      return;
-    }
-
-    for (let i = index; i < arr.length; i++) {
-      cur_arr.push(arr[i]);
-      combination(cur_arr, i + 1);
-      cur_arr.pop();
-    }
+    answer.add(arr.join(""));
+    return;
   }
 
-  combination([], 0);
-  return result;
+  const { start, end } = parentheses_index[depth];
+
+  arr[start] = "(";
+  arr[end] = ")";
+  dfs(depth + 1, arr, index + 1);
+
+  arr[start] = arr[end] = "";
+  dfs(depth + 1, arr, index);
 }
+
+dfs(0, [...input], 0);
+
+answer = [...answer].sort();
+console.log(answer.join("\n"));
