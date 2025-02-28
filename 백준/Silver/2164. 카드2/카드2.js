f951337edc85b38150afd523cc1ac0139e666d3b
A_jin <77830408+ppochaco@users.noreply.github.com>
2025-02-28 19:48:37 +0900

    [Silver IV] Title: 카드2, Time: 364 ms, Memory: 57704 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.js
@@ -0,0 +1,55 @@
+const input = require("fs")
+  .readFileSync(
+    process.platform === "linux" ? "/dev/stdin" : __dirname + "/input.txt"
+  )
+  .toString()
+  .trim();
+
+class Queue {
+  constructor() {
+    this.storage = new Object();
+    this.front = 0;
+    this.rear = 0;
+  }
+
+  size() {
+    return this.rear - this.front;
+  }
+
+  push(element) {
+    this.storage[this.rear] = element;
+    this.rear++;
+  }
+
+  pop() {
+    const removed = this.storage[this.front];
+    delete this.storage[this.front];
+    this.front++;
+
+    if (this.front === this.rear) {
+      this.front = 0;
+      this.rear = 0;
+    }
+
+    return removed;
+  }
+}
+
+const n = Number(input);
+const queue = new Queue();
+
+for (let i = 1; i <= n; i++) {
+  queue.push(i);
+}
+
+while (queue.size() !== 1) {
+  queue.pop();
+
+  if (queue.size() === 1) {
+    break;
+  }
+
+  queue.push(queue.pop());
+}
+
+console.log(queue.pop());
