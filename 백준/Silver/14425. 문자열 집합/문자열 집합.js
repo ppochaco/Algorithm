b70ee169f9187506a83b4c5d799f6ad31e13434c
A_jin <77830408+ppochaco@users.noreply.github.com>
2025-03-09 15:18:57 +0900

    [Silver IV] Title: 문자열 집합, Time: 132 ms, Memory: 30808 KB -BaekjoonHub

--- 백준/Silver/14425. 문자열 집합/문자열 집합.js
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.js
@@ -8,21 +8,16 @@ const input = require("fs")
 let index = 0;
 const [n, m] = input[index++].split(" ").map(Number);
 
-const s = new Map();
+const s = new Set();
 for (let i = 0; i < n; i++) {
-  s.set(input[index++], 0);
-}
-
-for (let i = 0; i < m; i++) {
-  const str = input[index++];
-  if (s.get(str) !== undefined) {
-    s.set(str, s.get(str) + 1);
-  }
+  s.add(input[index++]);
 }
 
 let answer = 0;
-for (let [_, cnt] of s) {
-  answer += cnt;
+for (let i = 0; i < m; i++) {
+  if (s.has(input[index++])) {
+    answer++;
+  }
 }
 
 console.log(answer);
