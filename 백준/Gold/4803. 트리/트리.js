b4eb65f27ab45566f8db4d646fd3d2431ed5efc5
A_jin <77830408+ppochaco@users.noreply.github.com>
2025-03-09 14:53:59 +0900

    [Gold IV] Title: 트리, Time: 536 ms, Memory: 51504 KB -BaekjoonHub

--- 백준/Gold/4803. 트리/트리.js
+++ 백준/Gold/4803. 트리/트리.js
@@ -11,29 +11,53 @@ let case_num = 1;
 
 while (true) {
   const [n, m] = input[index++].split(" ").map(Number);
-  const graph = Array.from({ length: n + 1 }, () => []);
-  let answer = 0;
 
   if (n + m === 0) {
     break;
   }
 
+  const parent = Array.from({ length: n + 1 }, (_, index) => index);
+  const cycle = new Set();
+
+  function find(node) {
+    if (parent[node] === node) return node;
+
+    parent[node] = find(parent[node]);
+    return parent[node];
+  }
+
+  function union(a, b) {
+    a = find(a);
+    b = find(b);
+
+    if (a < b) parent[b] = a;
+    else parent[a] = b;
+  }
+
   for (let i = 0; i < m; i++) {
     const [a, b] = input[index++].split(" ").map(Number);
 
-    graph[a].push(b);
-    graph[b].push(a);
-  }
-
-  const visited = Array.from({ length: n + 1 }, () => false);
-
-  for (let j = 1; j <= n; j++) {
-    if (!visited[j]) {
-      const is_tree = visit_tree(graph, j, visited);
-
-      if (is_tree) answer++;
+    if (find(a) === find(b)) {
+      cycle.add(parent[a]);
     }
+
+    if (cycle.has(parent[a]) || cycle.has(parent[b])) {
+      cycle.add(parent[a]);
+      cycle.add(parent[b]);
+    }
+
+    union(a, b);
   }
+
+  for (let i = 1; i <= n; i++) {
+    find(i);
+  }
+
+  const parent_set = new Set(parent.slice(1));
+  const intersection = new Set(
+    [...parent_set].filter((node) => cycle.has(node))
+  );
+  const answer = parent_set.size - intersection.size;
 
   if (answer === 1) {
     console.log(`Case ${case_num}: There is one tree.`);
@@ -44,32 +68,4 @@ while (true) {
   }
 
   case_num++;
-}
-
-function visit_tree(graph, start_node, visited) {
-  const queue = [];
-
-  queue.push(start_node);
-
-  let is_tree = true;
-
-  while (queue.length) {
-    const node = queue.shift();
-
-    if (visited[node]) {
-      is_tree = false;
-    }
-
-    visited[node] = true;
-
-    for (let i = 0; i < graph[node].length; i++) {
-      const next_node = graph[node][i];
-
-      if (!visited[next_node]) {
-        queue.push(next_node);
-      }
-    }
-  }
-
-  return is_tree;
 }
