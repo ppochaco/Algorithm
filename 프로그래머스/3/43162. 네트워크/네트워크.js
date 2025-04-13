622f719fb6e2eb7684c5e0290041b2ac890a1cb7
A_jin <77830408+ppochaco@users.noreply.github.com>
2025-04-13 14:49:54 +0900

    [level 3] Title: 네트워크, Time: 0.78 ms, Memory: 36.9 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.js
+++ 프로그래머스/3/43162. 네트워크/네트워크.js
@@ -2,28 +2,23 @@ function solution(n, computers) {
     var answer = 0;
     const visited = Array.from({ length: n }, () => false);
     
-    function bfs(start) {
-        visited[start] = true
-        answer ++
-        
-        const queue = [start];
-        while(queue.length) {
-            const computer = queue.shift();
-            for(let i = 0; i < n; i++) {
-                if (computers[computer][i] === 0) continue;
-                
-                if (!visited[i]) {
-                    queue.push(i)
-                    visited[i] = true
-                }
-            }
-        }
+   function dfs(cur) {
+       if (visited[cur]) {
+           return 0
+       }
+       
+       visited[cur] = true
+       for (let i = 0; i < n; i++) {
+           if (computers[cur][i]) {
+               dfs(i)
+           }
+       }
+       
+       return 1
     }
-    
-    for(let i = 0; i < n; i++) {
-        if (!visited[i]) {
-            bfs(i)
-        }
+
+    for (let i = 0; i < n; i ++) {
+        answer += dfs(i)
     }
     
     return answer;
