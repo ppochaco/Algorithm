b697e0143996f6b027ec6f7dd63f702428be670b
A_jin <77830408+ppochaco@users.noreply.github.com>
2025-04-24 21:46:40 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.03 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.cpp
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.cpp
@@ -2,51 +2,48 @@
 #include <vector>
 #include <iostream>
 #include <algorithm>
+#include <cstring>
 
 using namespace std;
 
 string solution(string new_id) {
-    for (int i = 0; i < new_id.size(); i++) {
-        char c = new_id[i];
+    
+    string temp;
+    for (char& c : new_id) {
         if ('A' <= c && c <= 'Z')
-            new_id[i] = c - 'A' + 'a';
-        else if ('a' <= c && c <= 'z')
-            continue;
-        else if ('0' <= c && c <= '9')
-            continue;
-        else if (c == '-' || c == '_' || c == '.')
-            continue;
-        else
-            new_id[i] = ' ';
+            temp += c - 'A' + 'a';
+        else if (
+            'a' <= c && c <= 'z' ||
+            '0' <= c && c <= '9' ||
+            strchr("-_.", c)
+        )
+            temp += c;
     }
-    new_id.erase(remove(new_id.begin(), new_id.end(), ' '), new_id.end());
+    new_id = temp;
     
-    char prev = ' ';
-    for (int i = 0; i < new_id.size(); i++) {
-        if (prev == '.' && new_id[i] == '.') {
-            new_id[i - 1] = ' ';
-        }
-        prev = new_id[i];
+    temp.clear();
+    for (char& c : new_id) {
+        if (!temp.empty() && temp.back() == '.' && c == '.')
+            continue;
+        temp += c;
     }
-    new_id.erase(remove(new_id.begin(), new_id.end(), ' '), new_id.end());
+    new_id = temp;
     
-    if (new_id[0] == '.')
-        new_id[0] = ' ';
-    if (new_id[new_id.size() - 1] == '.')
-        new_id[new_id.size() - 1] = ' ';
-    new_id.erase(remove(new_id.begin(), new_id.end(), ' '), new_id.end());
+    if (new_id.front() == '.')
+        new_id.erase(new_id.begin());
+    if (new_id.back() == '.')
+        new_id.pop_back();
     
-    if (new_id.size() == 0)
+    if (new_id.empty())
         new_id = 'a';
     
     if (new_id.size() >= 16)
-        new_id.erase(15, new_id.size());
-    
-    if (new_id[new_id.size() - 1] == '.')
+        new_id = new_id.substr(0, 15);
+    if (new_id.back() == '.')
         new_id.pop_back();
     
     while (new_id.size() <= 2) {
-        new_id.push_back(new_id[new_id.size() - 1]);
+        new_id += new_id.back();
     }
     
     return new_id;
