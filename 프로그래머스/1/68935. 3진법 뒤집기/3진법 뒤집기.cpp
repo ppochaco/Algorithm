f87d360cae4885cb4ac1661f793cd279aaa868db
A_jin <77830408+ppochaco@users.noreply.github.com>
2025-04-24 20:30:02 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.cpp
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.cpp
@@ -2,32 +2,22 @@
 #include <vector>
 #include <cmath>
 #include <iostream>
-#include <algorithm> 
 
 using namespace std;
 
 int solution(int n) {
     int answer = 0;
-    
-    int k = 0;
-    while(pow(3, k + 1) <= n) {
-        k++;
-    }
-    
+
     vector<int> digits;
-    while(k >= 0) {
-        int three = pow(3, k);
-        int a = n / three;
-        int b = n % three;
-        
-        digits.push_back(a);
-        n = b;
-        k--;
+    while(n > 0) {
+        digits.push_back(n % 3);
+        n = n / 3;
     }
-    reverse(digits.begin(), digits.end());
     
-    for (int i = 0; i < digits.size(); i++) {      
-        answer += pow(3, digits.size() - 1 - i) * (int)digits[i];
+    int k = 1;
+    for (int i = digits.size() - 1; i >= 0; i--) {      
+        answer += k * digits[i];
+        k *= 3;
     }
     
     return answer;
