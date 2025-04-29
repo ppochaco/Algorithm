c405e9774d7645810ee1bb83a826afffb6863ae3
A_jin <77830408+ppochaco@users.noreply.github.com>
2025-04-29 18:38:36 +0900

    [Gold V] Title: AC, Time: 316 ms, Memory: 51044 KB -BaekjoonHub

--- 백준/Gold/5430. AC/AC.js
+++ 백준/Gold/5430. AC/AC.js
@@ -8,7 +8,7 @@ const input = require("fs")
 let index = 0;
 let t = Number(input[index++]);
 
-class Queue {
+class Deque {
   constructor() {
     this.storage = new Object();
     this.front = 0;
@@ -16,30 +16,28 @@ class Queue {
     this.reverse = false;
   }
 
-  size() {
-    return this.rear - this.front;
-  }
-
   push(element) {
     this.storage[this.rear] = element;
     this.rear++;
   }
 
-  pop() {
-    if (this.rear === 0) return undefined;
+  shift() {
+    if (this.rear === this.front) return undefined;
 
-    if (this.reverse) {
-      this.rear--;
-      let removed = this.storage[this.rear];
-      delete this.storage[this.rear];
+    this.rear--;
+    let removed = this.storage[this.rear];
+    delete this.storage[this.rear];
 
-      if (this.front === this.rear) {
-        this.front = 0;
-        this.rear = 0;
-      }
-
-      return removed;
+    if (this.front === this.rear) {
+      this.front = 0;
+      this.rear = 0;
     }
+
+    return removed;
+  }
+
+  pop() {
+    if (this.rear === this.front) return undefined;
 
     let removed = this.storage[this.front];
     delete this.storage[this.front];
@@ -58,47 +56,44 @@ while (t--) {
   const p = input[index++];
   const n = Number(input[index++]);
   let number = input[index++];
-  number = n
-    ? number
-        .slice(1, number.length - 1)
-        .split(",")
-        .map(Number)
-    : [];
+  number = number.slice(1, number.length - 1).split(",");
 
   console.log(AC(p, number, n));
 }
 
 function AC(p, number, n) {
-  let queue = new Queue();
+  let deque = new Deque();
 
   for (let i = 0; i < n; i++) {
-    queue.push(number[i]);
+    deque.push(number[i]);
   }
 
   for (let i = 0; i < p.length; i++) {
     const command = p[i];
 
     if (command == "R") {
-      queue.reverse = !queue.reverse;
+      deque.reverse = !deque.reverse;
     } else if (command == "D") {
-      if (queue.pop() === undefined) {
+      const removed = deque.reverse ? deque.shift() : deque.pop();
+
+      if (removed === undefined) {
         return "error";
       }
     }
   }
 
-  let result = "";
-  if (queue.reverse) {
-    for (let i = queue.rear - 1; i >= queue.front; i--) {
-      result += queue.storage[i].toString() + ",";
+  let result = [];
+  if (deque.reverse) {
+    for (let i = deque.rear - 1; i >= deque.front; i--) {
+      result.push(deque.storage[i]);
     }
   } else {
-    for (let i = queue.front; i < queue.rear; i++) {
-      result += queue.storage[i].toString() + ",";
+    for (let i = deque.front; i < deque.rear; i++) {
+      result.push(deque.storage[i]);
     }
   }
 
-  result = "[" + result.slice(0, -1) + "]";
+  result = "[" + result.join(",") + "]";
 
   return result;
 }
