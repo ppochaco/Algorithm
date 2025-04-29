bac6236e1a283f15a12e9a7ec8296b9891a1a19d
A_jin <77830408+ppochaco@users.noreply.github.com>
2025-04-29 17:27:53 +0900

    [Silver III] Title: 계단 오르기, Time: 92 ms, Memory: 9344 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.js
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.js
@@ -7,23 +7,22 @@ const input = require("fs")
 
 let index = 0;
 const n = Number(input[index++]);
-const stairs = [];
+const stairs = [0];
 
 for (let i = 0; i < n; i++) {
   stairs.push(Number(input[index++]));
 }
 
-const dp = [0 * n];
+const dp = [0 * (n + 1)];
 
-dp[0] = stairs[0];
-dp[1] = dp[0] + stairs[1];
-dp[2] = Math.max(stairs[1] + stairs[2], dp[0] + stairs[2]);
+dp[1] = stairs[1];
+dp[2] = dp[1] + stairs[2];
 
-for (let i = 3; i < n; i++) {
+for (let i = 3; i <= n; i++) {
   dp[i] = Math.max(
     dp[i - 3] + stairs[i - 1] + stairs[i],
     dp[i - 2] + stairs[i]
   );
 }
 
-console.log(dp[n - 1]);
+console.log(dp[n]);
