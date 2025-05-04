6fdfc00c960a76383fd514e67b0fe391c274e419
A_jin <77830408+ppochaco@users.noreply.github.com>
2025-05-04 17:57:20 +0900

    [Silver III] Title: N과 M (6), Time: 92 ms, Memory: 9596 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.js
@@ -0,0 +1,34 @@
+const input = require("fs")
+  .readFileSync(
+    process.platform === "linux" ? "/dev/stdin" : __dirname + "/input.txt"
+  )
+  .toString()
+  .split("\n");
+
+const [n, m] = input[0].split(" ").map(Number);
+const nums = input[1].split(" ").map(Number);
+nums.sort((a, b) => a - b);
+
+let answer = [];
+const visited = [false * n];
+
+function permutation(index, cur) {
+  if (cur.length === m) {
+    answer.push(cur.join(" "));
+    return;
+  }
+
+  for (let i = index; i < n; i++) {
+    if (!visited[i]) {
+      cur.push(nums[i]);
+      visited[i] = true;
+      permutation(i + 1, cur);
+      cur.pop();
+      visited[i] = false;
+    }
+  }
+}
+
+permutation(0, []);
+
+console.log(answer.join("\n"));
