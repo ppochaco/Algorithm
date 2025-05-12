05821b4d173554f62fe20f1461678d589e603ab7
A_jin <77830408+ppochaco@users.noreply.github.com>
2025-05-12 16:57:05 +0900

    [Gold III] Title: 벽 부수고 이동하기 2, Time: 4536 ms, Memory: 301176 KB -BaekjoonHub

--- 백준/Gold/14442. 벽 부수고 이동하기 2/벽 부수고 이동하기 2.js
+++ 백준/Gold/14442. 벽 부수고 이동하기 2/벽 부수고 이동하기 2.js
@@ -1,44 +1,3 @@
-class Node {
-  constructor(data) {
-    this.data = data;
-    this.next = null;
-  }
-}
-
-class Queue {
-  constructor() {
-    this.head = null;
-    this.rear = null;
-    this.length = 0;
-  }
-
-  size() {
-    return this.length;
-  }
-
-  push(element) {
-    const node = new Node(element);
-
-    if (!this.head) {
-      this.head = node;
-    } else {
-      this.rear.next = node;
-    }
-
-    this.rear = node;
-    this.length++;
-  }
-
-  shift() {
-    if (!this.head) return undefined;
-
-    const removed = this.head.data;
-    this.head = this.head.next;
-    this.length--;
-
-    return removed;
-  }
-}
 const input = require("fs")
   .readFileSync(
     process.platform === "linux" ? "/dev/stdin" : __dirname + "/input.txt"
@@ -66,35 +25,38 @@ function bfs() {
   const visited = Array.from({ length: n }, () =>
     Array.from({ length: m }, () => Array(k + 1).fill(0))
   );
-  const queue = new Queue();
-  queue.push([0, 0, 0]);
   visited[0][0][0] = 1;
 
-  while (queue.size()) {
-    const [broke, x, y] = queue.shift();
+  let queue = [[0, 0, 0]];
+  while (queue.length) {
+    const next_queue = [];
 
-    if (x === n - 1 && y === m - 1) {
-      return visited[x][y][broke];
-    }
+    for (const [broke, x, y] of queue) {
+      if (x === n - 1 && y === m - 1) {
+        return visited[x][y][broke];
+      }
 
-    for (const [dx, dy] of directions) {
-      const nx = x + dx;
-      const ny = y + dy;
+      for (const [dx, dy] of directions) {
+        const nx = x + dx;
+        const ny = y + dy;
 
-      if (nx < 0 || ny < 0 || nx >= n || ny >= m) continue;
+        if (nx < 0 || ny < 0 || nx >= n || ny >= m) continue;
 
-      if (map[nx][ny] === 0 && visited[nx][ny][broke] === 0) {
-        visited[nx][ny][broke] = visited[x][y][broke] + 1;
-        queue.push([broke, nx, ny]);
-      } else if (
-        map[nx][ny] === 1 &&
-        broke < k &&
-        visited[nx][ny][broke + 1] === 0
-      ) {
-        visited[nx][ny][broke + 1] = visited[x][y][broke] + 1;
-        queue.push([broke + 1, nx, ny]);
+        if (map[nx][ny] === 0 && visited[nx][ny][broke] === 0) {
+          visited[nx][ny][broke] = visited[x][y][broke] + 1;
+          next_queue.push([broke, nx, ny]);
+        } else if (
+          map[nx][ny] === 1 &&
+          broke < k &&
+          visited[nx][ny][broke + 1] === 0
+        ) {
+          visited[nx][ny][broke + 1] = visited[x][y][broke] + 1;
+          next_queue.push([broke + 1, nx, ny]);
+        }
       }
     }
+
+    queue = next_queue;
   }
 
   return -1;
