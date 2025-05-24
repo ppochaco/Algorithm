c9435fd188a4a0582d6421a3728b2832161cdb2f
A_jin <77830408+ppochaco@users.noreply.github.com>
2025-05-25 00:15:57 +0900

    [Gold II] Title: 새로운 게임 2, Time: 172 ms, Memory: 12536 KB -BaekjoonHub

--- 백준/Gold/17837. 새로운 게임 2/새로운 게임 2.js
+++ 백준/Gold/17837. 새로운 게임 2/새로운 게임 2.js
@@ -10,51 +10,40 @@ const board = [];
 for (let i = 0; i < n; i++) {
   board.push(input[index++].split(" ").map(Number));
 }
-const horse = Array.from({ length: n }, () => Array(n).fill(null));
-const horse_position = Array(k + 1).fill([]);
+const horse = Array.from({ length: n }, () =>
+  Array.from({ length: n }, () => [])
+);
+
+const horse_position = Array(k + 1).fill(null);
 for (let i = 0; i < k; i++) {
   const [x, y, d] = input[index++].split(" ").map(Number);
-  if (horse[x - 1][y - 1]) horse[x - 1][y - 1].push([{ number: i + 1, d }]);
-  else horse[x - 1][y - 1] = [{ number: i + 1, d }];
-
-  horse_position[i + 1] = [x - 1, y - 1, d];
+  horse[x - 1][y - 1].push(i + 1);
+  horse_position[i + 1] = { x: x - 1, y: y - 1, d };
 }
+
 const MAX_TURN = 1000;
 const dx = [0, 0, 0, -1, 1];
 const dy = [0, 1, -1, 0, 0];
 const reverse_d = { 1: 2, 2: 1, 3: 4, 4: 3 };
 
-let turn = 1;
-let i = 1;
-while (turn <= MAX_TURN) {
-  const [x, y, d] = horse_position[i];
-  const horse_cnt = move_horse(x, y, i, d);
+console.log(game());
 
-  if (4 <= horse_cnt) {
-    break;
-  }
+function game() {
+  for (let turn = 1; turn <= 1000; turn++) {
+    for (let number = 1; number <= k; number++) {
+      move_horse(number, horse_position[number]);
+      const { x, y } = horse_position[number];
 
-  i++;
-  if (i === k + 1) {
-    i = 1;
-    turn++;
-  }
-}
-
-if (MAX_TURN <= turn) console.log(-1);
-else console.log(turn);
-
-function move_horse(x, y, number, d) {
-  const horses = [];
-
-  while (true) {
-    const cur = horse[x][y].pop();
-    horses.push(cur);
-    if (cur.number === number) {
-      break;
+      if (4 <= horse[x][y].length) {
+        return turn;
+      }
     }
   }
 
+  return -1;
+}
+
+function move_horse(number, { x, y, d }) {
   let nx = x + dx[d];
   let ny = y + dy[d];
 
@@ -62,46 +51,25 @@ function move_horse(x, y, number, d) {
     d = reverse_d[d];
     nx = x + dx[d];
     ny = y + dy[d];
-    horses[horses.length - 1] = { number, d };
+    horse_position[number].d = d;
   }
 
   if (is_blue()) {
-    for (let i = horses.length - 1; i >= 0; i--) {
-      horse[x][y].push(horses[i]);
-    }
-
-    return horse[x][y].length;
+    return;
   }
 
-  if (board[nx][ny] === 0) {
-    if (horse[nx][ny]) {
-      for (let i = horses.length - 1; i >= 0; i--) {
-        horse[nx][ny].push(horses[i]);
-        horse_position[horses[i].number] = [nx, ny, horses[i].d];
-      }
-    } else {
-      horse[nx][ny] = [];
-      for (let i = horses.length - 1; i >= 0; i--) {
-        horse[nx][ny].push(horses[i]);
-        horse_position[horses[i].number] = [nx, ny, horses[i].d];
-      }
-    }
-  } else if (board[nx][ny] === 1) {
-    if (horse[nx][ny]) {
-      for (let i = 0; i < horses.length; i++) {
-        horse[nx][ny].push(horses[i]);
-        horse_position[horses[i].number] = [nx, ny, horses[i].d];
-      }
-    } else {
-      horse[nx][ny] = [];
-      for (let i = 0; i < horses.length; i++) {
-        horse[nx][ny].push(horses[i]);
-        horse_position[horses[i].number] = [nx, ny, horses[i].d];
-      }
-    }
+  let horse_stack = horse[x][y].splice(horse[x][y].indexOf(number));
+
+  for (let cur_horse of horse_stack) {
+    horse_position[cur_horse].x = nx;
+    horse_position[cur_horse].y = ny;
   }
 
-  return horse[nx][ny].length;
+  if (board[nx][ny] === 1) {
+    horse_stack = horse_stack.reverse();
+  }
+
+  horse[nx][ny] = horse[nx][ny].concat(horse_stack);
 
   function is_blue() {
     if (nx < 0 || ny < 0 || nx >= n || ny >= n) return true;
