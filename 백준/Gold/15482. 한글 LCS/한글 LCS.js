27bf15f6a2d86c52f5a2102d689e7c96b1711fc2
A_jin <77830408+ppochaco@users.noreply.github.com>
2025-06-05 10:31:31 +0900

    [Gold V] Title: 한글 LCS, Time: 168 ms, Memory: 11128 KB -BaekjoonHub

--- 백준/Gold/15482. 한글 LCS/한글 LCS.js
+++ 백준/Gold/15482. 한글 LCS/한글 LCS.js
@@ -8,18 +8,18 @@ const input = require("fs")
 const a = input[0];
 const b = input[1];
 
-const dp = Array.from({ length: a.length + 1 }, () =>
-  Array(b.length + 1).fill(0)
-);
+const dp = Array(b.length).fill(0);
 
-for (let i = 1; i <= a.length; i++) {
-  for (let j = 1; j <= b.length; j++) {
-    if (a[i - 1] === b[j - 1]) {
-      dp[i][j] = dp[i - 1][j - 1] + 1;
-    } else {
-      dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+let cnt = 0;
+for (let i = 0; i < a.length; i++) {
+  cnt = 0;
+  for (let j = 0; j < b.length; j++) {
+    if (cnt < dp[j]) {
+      cnt = dp[j];
+    } else if (a[i] === b[j]) {
+      dp[j] = cnt + 1;
     }
   }
 }
 
-console.log(dp[a.length][b.length]);
+console.log(Math.max(...dp));
