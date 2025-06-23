9f618c80ba9d8d1ddc77ded7c04c3ba15ec7c0b1
A_jin <77830408+ppochaco@users.noreply.github.com>
2025-06-23 09:56:52 +0900

    [Gold IV] Title: 스도쿠, Time: 1800 ms, Memory: 89704 KB -BaekjoonHub

--- 백준/Gold/2580. 스도쿠/스도쿠.js
+++ 백준/Gold/2580. 스도쿠/스도쿠.js
@@ -14,6 +14,7 @@ for (let i = 0; i < n; i++) {
 const row = Array.from({ length: n }, () => new Set());
 const col = Array.from({ length: n }, () => new Set());
 const square = Array.from({ length: n }, () => new Set());
+const getSqureIndex = (x, y) => Math.floor(x / 3) * 3 + Math.floor(y / 3);
 
 const zero = [];
 for (let i = 0; i < n; i++) {
@@ -21,42 +22,39 @@ for (let i = 0; i < n; i++) {
     if (board[i][j]) {
       row[i].add(board[i][j]);
       col[j].add(board[i][j]);
-      square[Math.floor(i / 3) * 3 + Math.floor(j / 3)].add(board[i][j]);
+      square[getSqureIndex(i, j)].add(board[i][j]);
     } else {
       zero.push([i, j]);
     }
   }
 }
 
-let check = false;
 dfs(0);
 
 function dfs(index) {
-  if (check) return;
-
   if (index === zero.length) {
     check = true;
     for (let i = 0; i < n; i++) {
       console.log(board[i].join(" "));
     }
 
-    return;
+    process.exit(0);
   }
 
   const [x, y] = zero[index];
   for (let i = 1; i <= n; i++) {
     if (row[x].has(i)) continue;
     if (col[y].has(i)) continue;
-    if (square[Math.floor(x / 3) * 3 + Math.floor(y / 3)].has(i)) continue;
+    if (square[getSqureIndex(x, y)].has(i)) continue;
 
     board[x][y] = i;
     row[x].add(i);
     col[y].add(i);
-    square[Math.floor(x / 3) * 3 + Math.floor(y / 3)].add(i);
+    square[getSqureIndex(x, y)].add(i);
     dfs(index + 1);
     board[x][y] = 0;
     row[x].delete(i);
     col[y].delete(i);
-    square[Math.floor(x / 3) * 3 + Math.floor(y / 3)].delete(i);
+    square[getSqureIndex(x, y)].delete(i);
   }
 }
