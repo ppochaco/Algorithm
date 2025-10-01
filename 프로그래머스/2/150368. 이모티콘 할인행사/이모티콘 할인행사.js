b88ae59ab9f4641288a3c4ab73f7c2c714bb626f
A_jin <77830408+ppochaco@users.noreply.github.com>
2025-10-02 00:45:33 +0900

    [level 2] Title: 이모티콘 할인행사, Time: 82.76 ms, Memory: 37.7 MB -BaekjoonHub

--- 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.js
+++ 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.js
@@ -1,47 +1,22 @@
 function solution(users, emoticons) {
-    const answer = { plus: 0, price: 0 };
+    const answer = [];
     
-    combi(0, []);
+    const prices = []
+    combi(prices, 0, []);
+    console.log(prices);
     
-    return [answer.plus, answer.price];
     
-    function combi(depth, arr) {
+    return answer;
+    
+    function combi(result, depth, arr) {
         if (depth === emoticons.length) {
-            const { plus, price } = calculate_price(arr);
-            
-            if (answer.plus < plus) {
-                answer.plus = plus;
-                answer.price = price;
-            } else if (answer.plus === plus && answer.price < price) {
-                answer.price = price;
-            }
-            
+            result.push([...arr]);
             return;
         }
         
         for (sale of [10, 20, 30, 40]) {
-            combi(depth + 1, [...arr, sale]);
+            combi(result, depth + 1, [...arr, sale]);
         }
-    }
-    
-    function calculate_price(sales) {
-        const result = { plus: 0, price: 0 };
-        for (const [percent, price] of users) {
-            let total_price = 0;
-            sales.forEach((sale, i) => {
-                if (percent <= sale) {
-                    total_price += emoticons[i] * (100 - sale) / 100;
-                }
-            })
-
-            if (price <= total_price) {
-                result.plus += 1;
-            } else {
-                result.price += total_price;
-            }
-        }
-
-        return result;
     }
 }
 
