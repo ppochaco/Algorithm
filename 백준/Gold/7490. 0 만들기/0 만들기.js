705cdbfda93be1c6f638010f2592a7f9f2f668e0
A_jin <77830408+ppochaco@users.noreply.github.com>
2025-10-08 22:35:48 +0900

    [Gold V] Title: 0 만들기, Time: 1492 ms, Memory: 17140 KB -BaekjoonHub

---
+++ 백준/Gold/7490. 0 만들기/0 만들기.js
@@ -0,0 +1,38 @@
+const input = require('fs')
+  .readFileSync(
+    process.platform === 'linux' ? '/dev/stdin' : __dirname + '/input.txt'
+  )
+  .toString()
+  .split('\n')
+  .map(Number);
+
+const targets = new Set(input);
+const n = Math.max(...input);
+const result = Array.from({ length: n + 1 }, () => []);
+
+dfs(1, '');
+
+const answer = [];
+result.forEach((cur) => {
+  if (cur.length) answer.push(cur.join('\n'));
+})
+console.log(answer.join('\n\n'));
+
+
+function dfs(index, expression) {
+  if (n < index) return;
+
+  expression += index;
+  if (targets.has(index) && is_zero(expression)) {
+    result[index].push(expression);
+  }
+  dfs(index + 1, expression + ' ');
+  dfs(index + 1, expression + '+');
+  dfs(index + 1, expression + '-');
+}
+
+function is_zero(expression) {
+  const result = eval(expression.replaceAll(" ", ""));
+  if (result === 0) return true;
+  return false;
+}
