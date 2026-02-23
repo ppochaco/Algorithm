6bbe1f398bdd034c790071bcc06e15c50330b650
A_jin <77830408+ppochaco@users.noreply.github.com>
2026-02-24 00:25:19 +0900

    [Silver III] Title: 숫자 야구, Time: 152 ms, Memory: 11712 KB -BaekjoonHub

--- 백준/Silver/2503. 숫자 야구/숫자 야구.js
+++ 백준/Silver/2503. 숫자 야구/숫자 야구.js
@@ -1,50 +1,61 @@
-const input = require('fs').readFileSync(process.platform === 'linux' ? '/dev/stdin' : __dirname + '/input.txt').toString().split('\n')
+const input = require("fs")
+  .readFileSync(
+    process.platform === "linux" ? "/dev/stdin" : __dirname + "/input.txt",
+  )
+  .toString()
+  .split("\n");
 
-const n = Number(input[0])
-const question = []
+const n = Number(input[0]);
+const question = [];
 for (let i = 1; i < n + 1; i++) {
-  question.push(input[i].split(' ').map(Number))
+  question.push(input[i].split(" ").map(Number));
 }
 
-const nums = Array.from({ length: 9 }, (_, index) => index + 1)
-let answer = 0
-for (const a of nums) {
-  for (const b of nums) {
-    if (a === b) continue
+let answer = 0;
+const N = 9;
+const R = 3;
+const checked = Array.from({ length: N + 1 }, () => false);
 
-    for (const c of nums) {
-      if (c === a || c === b) continue
-      
-      if (check_question(a, b, c)) answer++
+permutation([], 0);
+console.log(answer);
+
+function permutation(cur, index) {
+  if (cur.length === R) {
+    if (check_question(cur)) answer++;
+    return;
+  }
+
+  for (let i = 1; i < N + 1; i++) {
+    if (checked[i]) continue;
+
+    cur.push(i);
+    checked[i] = true;
+
+    permutation(cur, index + 1);
+
+    cur.pop();
+    checked[i] = false;
+  }
+}
+
+function check_question(abc) {
+  for (const [num, strike, ball] of question) {
+    const num_arr = String(num).split("").map(Number);
+    const [s, b] = get_strike_ball(abc, num_arr);
+    if (strike !== s || ball !== b) return false;
+  }
+
+  return true;
+}
+
+function get_strike_ball(abc, num) {
+  let strike = 0;
+  let ball = 0;
+  for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 3; j++) {
+      if (abc[i] === num[j]) i === j ? strike++ : ball++;
     }
   }
-}
-console.log(answer)
 
-function check_question(a, b, c) {
-  for (const [num, strike, ball] of question) {
-    const num_arr = String(num).split('').map(Number)
-    if (strike !== get_strike(a, b, c, num_arr)) return false
-    if (ball !== get_ball(a, b, c, num_arr)) return false
-  }
-
-  return true
-}
-
-function get_strike(a, b, c, num) {
-  let cnt = 0
-  if (a === num[0]) cnt++
-  if (b === num[1]) cnt++
-  if (c === num[2]) cnt++
-
-  return cnt
-}
-
-function get_ball(a, b, c, num) {
-  let cnt = 0
-  if (a !== num[0] && (a === num[1] || a === num[2])) cnt++
-  if (b !== num[1] && (b === num[2] || b === num[0])) cnt++
-  if (c !== num[2] && (c === num[0] || c === num[1])) cnt++
-
-  return cnt
+  return [strike, ball];
 }
