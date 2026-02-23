72b7784cd048a278b3e1528c1ab11692b18506f4
A_jin <77830408+ppochaco@users.noreply.github.com>
2026-02-23 17:28:04 +0900

    [Silver II] Title: 부분수열의 합, Time: 284 ms, Memory: 11232 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.js
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.js
@@ -1,41 +1,25 @@
-const input = require("fs")
-  .readFileSync(
-    process.platform === "linux" ? "/dev/stdin" : __dirname + "/input.txt"
-  )
-  .toString()
-  .split("\n");
+const input = require('fs').readFileSync(process.platform === 'linux' ? '/dev/stdin' : __dirname + '/input.txt').toString().split('\n')
 
-const [n, s] = input[0].split(" ").map(Number);
-const nums = input[1].split(" ").map(Number);
+const [n, s] = input[0].split(' ').map(Number)
+const nums = input[1].split(' ').map(Number)
+let answer = 0
 
-nums.sort((a, b) => a - b);
-
-function combination(arr, r) {
-  const result = [];
-
-  function combine(cur_arr, index) {
-    if (cur_arr.length === r) {
-      result.push([...cur_arr]);
-      return;
-    }
-
-    for (let i = index; i < arr.length; i++) {
-      cur_arr.push(arr[i]);
-      combine(cur_arr, i + 1);
-      cur_arr.pop(arr[i]);
-    }
-  }
-
-  combine([], 0);
-
-  return result;
+for (let i = 1; i < n + 1; i++) {
+  combination(nums, i, 0, [])
 }
+console.log(answer)
 
-let answer = 0;
-for (let i = 1; i <= n; i++) {
-  for (let combi of combination(nums, i)) {
-    if (s === combi.reduce((pre, cur) => pre + cur, 0)) answer++;
+function combination(arr, r, index, cur) {
+  if (cur.length === r) {
+    const sum = cur.reduce((a, b) => a + b)
+    if (sum === s) answer++
+    return
+  }
+  
+  for (let i = index; i < arr.length; i++) {
+    cur.push(arr[i])
+    combination(arr, r, i + 1, cur)
+    cur.pop()
   }
 }
 
-console.log(answer);
