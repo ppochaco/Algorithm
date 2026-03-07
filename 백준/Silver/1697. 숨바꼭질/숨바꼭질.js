e64d36325e9cc6824902a5c24eb26902bd1ebf3f
A_jin <77830408+ppochaco@users.noreply.github.com>
2026-03-07 17:57:15 +0900

    [Silver I] Title: 숨바꼭질, Time: 176 ms, Memory: 16076 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.js
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.js
@@ -1,94 +1,31 @@
 const input = require('fs').readFileSync(process.platform === 'linux' ? '/dev/stdin' : __dirname + '/input.txt').toString().split('\n')
 
-class PriorityQueue {
-  constructor() {
-    this.queue = []
-  }
-
-  size() {
-    return this.queue.length
-  }
-
-  push(value) {
-    this.queue.push(value)
-    this.#moveUp()
-  }
-
-  pop(){
-    if (!this.size()) return undefined
-    if (this.size() === 1) return this.queue.pop()
-
-    const top = this.queue[0]
-    this.queue[0] = this.queue.pop()
-    this.#moveDown()
-
-    return top
-  }
-
-  #swap(a, b) {
-    [this.queue[a], this.queue[b]] = [this.queue[b], this.queue[a]]
-  }
-
-  #moveUp() {
-    let index = this.size() - 1
-
-    while(index) {
-      const pIndex = Math.floor((index - 1) / 2)
-
-      if (this.queue[pIndex][0] < this.queue[index][0]) break
-      if (this.queue[pIndex][0] === this.queue[index][0] && this.queue[pIndex][1] <= this.queue[index][1]) break
-
-      this.#swap(index, pIndex)
-      index = pIndex
-    }
-  }
-
-  #moveDown() {
-    let index = 0
-    while(true) {
-      const left = index * 2 + 1
-      const right = index * 2 + 2
-      if (this.size() <= left) break
-
-      const sIndex = right < this.size() && this.queue[right][0] < this.queue[left][0] ? right : left
-      if (this.queue[index][0] <= this.queue[sIndex][0]) break
-
-      this.#swap(index, sIndex)
-      index = sIndex
-    }
-  }
-}
-
-
 const [n, k] = input[0].split(' ').map(Number)
-const distance =  Math.abs(k - n)
 const MAX_N = 100_000
+const visited = Array.from({ length: MAX_N + 1 }, () => -1)
 
-console.log(bfs())
+bfs()
+
+console.log(visited[k])
 
 function bfs() {
-  const visited = Array.from({ length: distance + 1 }, () => false)
-  const queue = new PriorityQueue()
-  visited[n] = true
-  queue.push([0, n])
+  let index = 0
+  const queue = []
 
-  while(queue.size()) {
-    const [time, position] = queue.pop()
-    
-    if (position === k) return time
-    if (distance <= time) return distance
+  visited[n] = 0
+  queue.push(n)
 
-    if (0 <= position - 1 && !visited[position - 1]) {
-      queue.push([time + 1, position - 1])
-      visited[position - 1] = true
-    }
-    if (position + 1 <= MAX_N && !visited[position + 1]) {
-      queue.push([time + 1, position + 1])
-      visited[position + 1] = true
-    }
-    if (position * 2 <= MAX_N && !visited[position * 2]) {
-      queue.push([time + 1, position * 2])
-      visited[position * 2] = true
+  while(queue.length) {
+    const position = queue[index++]
+    if (position === k) return
+
+    for (const d of [- 1, + 1, position]) {
+      const next_position = d + position
+      if (next_position < 0 || next_position > MAX_N) continue
+      if (visited[next_position] !== -1) continue
+
+      queue.push(next_position)
+      visited[next_position] = visited[position] + 1
     }
   }
 }
