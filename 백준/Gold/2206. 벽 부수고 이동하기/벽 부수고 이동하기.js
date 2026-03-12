4b1909f8ed3fc776f8620b6b3b2f4fc977783f47
A_jin <77830408+ppochaco@users.noreply.github.com>
2026-03-13 00:40:59 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 2476 ms, Memory: 348156 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.js
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.js
@@ -1,54 +1,44 @@
-const input = require("fs")
-  .readFileSync(
-    process.platform === "linux" ? "/dev/stdin" : __dirname + "/input.txt"
-  )
-  .toString()
-  .split("\n");
+const input = require('fs').readFileSync(process.platform === 'linux' ? '/dev/stdin' : __dirname + '/input.txt').toString().split('\n')
 
-let index = 0;
-const [n, m] = input[index++].split(" ").map(Number);
-const map = [];
-for (let i = 0; i < n; i++) {
-  map.push(input[index++].split("").map(Number));
+const [n, m] = input[0].split(' ').map(Number)
+const board = []
+for (let i = 1; i < n + 1; i++) {
+  board.push(input[i].split('').map(Number))
 }
 
-const directions = [
-  [0, 1],
-  [0, -1],
-  [1, 0],
-  [-1, 0],
-];
-
-console.log(bfs());
+const directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+console.log(bfs())
 
 function bfs() {
-  const visited = Array.from({ length: n }, () => Array(m).fill(-1));
-  visited[0][0] = 1;
-  const queue = [[1, 1, 0, 0]];
+  const visited = Array.from({ length: n } , () => Array.from({ length: m }, () => Array.from({ length: 2 }, () => 0)))
+  let idx = 0
+  const queue = []
 
-  let front = 0;
-  while (front < queue.length) {
-    const [w, distance, x, y] = queue[front++];
+  visited[0][0][0] = 1
+  queue.push([0, 0, 0])
 
-    if (x === n - 1 && y === m - 1) {
-      return distance;
+  while(idx < queue.length) {
+    const [x, y, broken] = queue[idx++]
+    if (x === n -1 && y === m - 1) {
+      return visited[x][y][broken]
     }
 
     for (const [dx, dy] of directions) {
-      const nx = x + dx;
-      const ny = y + dy;
-      let nw = w;
+      const [nx, ny] = [dx + x, dy + y]
+      
+      if (nx < 0 || ny < 0 || nx >= n || ny >= m) continue
+      if (visited[nx][ny][broken]) continue
 
-      if (nx < 0 || ny < 0 || nx >= n || ny >= m) continue;
-
-      if (map[nx][ny] === 1) nw = w - 1;
-
-      if (nw <= visited[nx][ny]) continue;
-
-      visited[nx][ny] = nw;
-      queue.push([nw, distance + 1, nx, ny]);
+      if (!board[nx][ny]) {
+        visited[nx][ny][broken] = visited[x][y][broken] + 1
+        queue.push([nx, ny, broken])
+      }
+      if (!broken && board[nx][ny]) {
+        visited[nx][ny][1] = visited[x][y][0] + 1
+        queue.push([nx, ny, 1])
+      }
     }
   }
 
-  return -1;
+  return -1
 }
