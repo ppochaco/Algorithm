7ae62a957e5463ecc055da4b3300596199fa83e0
A_jin <77830408+ppochaco@users.noreply.github.com>
2026-03-15 21:10:14 +0900

    [Gold IV] Title: 인구 이동, Time: 1436 ms, Memory: 28256 KB -BaekjoonHub

--- 백준/Gold/16234. 인구 이동/인구 이동.js
+++ 백준/Gold/16234. 인구 이동/인구 이동.js
@@ -1,69 +1,70 @@
-const input = require("fs")
-  .readFileSync(
-    process.platform === "linux" ? "/dev/stdin" : __dirname + "/input.txt"
-  )
-  .toString()
-  .split("\n");
+const input = require('fs').readFileSync(process.platform === 'linux' ? '/dev/stdin' : __dirname + '/input.txt').toString().split('\n')
 
-let index = 0;
-const [n, l, r] = input[index++].split(" ").map(Number);
-const board = [];
-for (let i = 0; i < n; i++) {
-  board.push(input[index++].split(" ").map(Number));
+const [n, l, r] = input[0].split(' ').map(Number)
+const board = []
+for (let i = 1; i < n + 1; i++) {
+  board.push(input[i].split(' ').map(Number))
 }
 
-const dx = [0, 1, 0, -1];
-const dy = [1, 0, -1, 0];
-let day = 0;
+const directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+let day = 0
 
-while (true) {
-  const visited = Array.from({ length: n }, () => Array(n).fill(false));
-  let blocked = 0;
+while(true) {
+  const visited = Array.from({ length: n }, () => Array.from({ length: n }, () => false))
+  let flag = false
 
   for (let i = 0; i < n; i++) {
     for (let j = 0; j < n; j++) {
-      if (!visited[i][j]) {
-        bfs(i, j);
-      }
+      if (visited[i][j]) continue
+    
+      if (bfs(i, j, visited)) flag = true
     }
   }
 
-  if (blocked === n * n) break;
+  if (!flag) break
 
-  day++;
-
-  function bfs(x, y) {
-    const queue = [[x, y]];
-    const country = [[x, y]];
-    let people = board[x][y];
-    visited[x][y] = true;
-
-    while (queue.length) {
-      const [x, y] = queue.pop();
-      for (let i = 0; i < 4; i++) {
-        const [nx, ny] = [x + dx[i], y + dy[i]];
-        if (nx < 0 || ny < 0 || nx >= n || ny >= n) continue;
-        if (visited[nx][ny]) continue;
-        const diff = Math.abs(board[nx][ny] - board[x][y]);
-        if (l <= diff && diff <= r) {
-          queue.push([nx, ny]);
-          country.push([nx, ny]);
-          people += board[nx][ny];
-          visited[nx][ny] = true;
-        }
-      }
-    }
-
-    if (country.length === 1) {
-      blocked++;
-    }
-
-    const people_per_country = Math.floor(people / country.length);
-    for (let i = 0; i < country.length; i++) {
-      const [x, y] = country[i];
-      board[x][y] = people_per_country;
-    }
-  }
+  day++
 }
 
-console.log(day);
+console.log(day)
+
+function bfs(startX, startY, visited) {
+  let population = 0
+  let area = 0
+  const positions = []
+
+  let idx = 0
+  const queue = []
+
+  queue.push([startX, startY])
+
+  while(idx < queue.length) {
+    const [x, y] = queue[idx++]
+
+    for (const [dx, dy] of directions) {
+      const [nx, ny] = [dx + x, dy + y]
+
+      if (nx < 0 || ny < 0 || nx >= n || ny >= n) continue
+      if (visited[nx][ny]) continue
+
+      const diff = Math.abs(board[nx][ny] - board[x][y])
+      if (diff < l || r < diff) continue
+
+      visited[nx][ny] = true
+      queue.push([nx, ny])
+
+      positions.push([nx, ny])
+      population += board[nx][ny]
+      area++
+    }
+  }
+
+  if (area === 0) return false
+
+  const average = Math.floor(population / area)
+  for (const [x, y] of positions) {
+    board[x][y] = average
+  }
+
+  return true
+}
