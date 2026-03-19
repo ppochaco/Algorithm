e50ba0a62041780d8b2539913801bb4bca41d905
A_jin <77830408+ppochaco@users.noreply.github.com>
2026-03-19 23:19:00 +0900

    [Silver IV] Title: 수들의 합 2, Time: 92 ms, Memory: 9784 KB -BaekjoonHub

--- 백준/Silver/2003. 수들의 합 2/수들의 합 2.js
+++ 백준/Silver/2003. 수들의 합 2/수들의 합 2.js
@@ -3,24 +3,20 @@ const input = require('fs').readFileSync(process.platform === 'linux' ? '/dev/st
 const [n, m] = input[0].split(' ').map(Number)
 const arr = input[1].split(' ').map(Number)
 
-const sum_arr = [0]
-let sum = 0
-for (const cur of arr) {
-  sum += cur
-  sum_arr.push(sum)
-}
-
 let answer = 0
 let left = 0
 let right = 0
+let sum = 0
 while(right < n + 1) {
-  const sum = sum_arr[right] - sum_arr[left]
-  
-  if (m === sum) {
-    answer++
+  if (sum <= m) {
+    sum += arr[right]
     right++
   }
-  else if (m < sum) left++
-  else if (m > sum) right++
+  else {
+    sum -= arr[left]
+    left++
+  }
+
+  if (m === sum) answer++
 }
 console.log(answer)
