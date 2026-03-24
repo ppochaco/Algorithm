3c9f582c93232f25841aff11c9674b13c8af4df8
A_jin <77830408+ppochaco@users.noreply.github.com>
2026-03-24 22:57:52 +0900

    [Gold V] Title: 수 고르기, Time: 228 ms, Memory: 22624 KB -BaekjoonHub

--- 백준/Gold/2230. 수 고르기/수 고르기.js
+++ 백준/Gold/2230. 수 고르기/수 고르기.js
@@ -1,32 +1,21 @@
-const input = require("fs")
-  .readFileSync(
-    process.platform === "linux" ? "/dev/stdin" : __dirname + "/input.txt"
-  )
-  .toString()
-  .split("\n");
+const input = require('fs').readFileSync(process.platform === 'linux' ? '/dev/stdin' : __dirname + '/input.txt').toString().trim().split('\n')
 
-let index = 0;
-const [n, m] = input[index++].split(" ").map(Number);
-const arr = [];
-for (let i = 0; i < n; i++) {
-  arr.push(Number(input[index++]));
-}
+const [N, M] = input.shift().split(' ').map(Number)
+const arr = input.map(Number)
+arr.sort((a, b) => a - b)
 
-arr.sort((a, b) => a - b);
+const MAX_DIFF = 2_000_000_000
+let answer = MAX_DIFF
+let left = 0
+let right = 0
+while (left <= right && right < N) {
+  const diff = Math.abs(arr[left] - arr[right])
 
-let start = 0;
-let end = 1;
-let answer = Math.max(...arr) - Math.min(...arr);
-while (start <= end && end < n) {
-  const distance = arr[end] - arr[start];
-  if (distance < m) end++;
-  else if (m < distance) {
-    start++;
-    answer = Math.min(answer, distance);
+  if (M <= diff) {
+    answer = Math.min(answer, diff)
+    left++
   } else {
-    answer = distance;
-    break;
+    right++
   }
 }
-
-console.log(answer);
+console.log(answer)
