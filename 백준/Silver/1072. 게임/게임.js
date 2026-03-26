13281ff9003ab61f9f2b4180d41ddd0559838aa9
A_jin <77830408+ppochaco@users.noreply.github.com>
2026-03-26 22:38:53 +0900

    [Silver III] Title: 게임, Time: 96 ms, Memory: 9604 KB -BaekjoonHub

--- 백준/Silver/1072. 게임/게임.js
+++ 백준/Silver/1072. 게임/게임.js
@@ -1,7 +1,7 @@
 const input = require('fs').readFileSync(process.platform === 'linux' ? '/dev/stdin' : __dirname + '/input.txt').toString().trim()
 
 const [X, Y] = input.split(' ').map(Number)
-const Z = Math.floor(Y / X * 1000 / 10)
+const Z = Math.floor(Y * 100 / X)
 
 const MAX = 1_000_000_000 + 1
 let answer = MAX
@@ -9,7 +9,7 @@ let start = 1
 let end = MAX
 while(start < end) {
   const cnt = Math.floor((start + end) / 2)
-  const z = Math.floor((Y + cnt) / (X + cnt) * 1000 / 10)
+  const z = Math.floor((Y + cnt) * 100 / (X + cnt))
 
   if (Z < z) {
     answer = cnt
