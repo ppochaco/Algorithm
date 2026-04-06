1e96e1f61e87acd91af173d3aa70afa61789a3f9
A_jin <77830408+ppochaco@users.noreply.github.com>
2026-04-06 22:32:43 +0900

    [Gold IV] Title: 타임머신, Time: 596 ms, Memory: 32140 KB -BaekjoonHub

--- 백준/Gold/11657. 타임머신/타임머신.py
+++ 백준/Gold/11657. 타임머신/타임머신.py
@@ -3,39 +3,30 @@ input = sys.stdin.readline
 
 n, m = map(int, input().split())
 graph = []
-
-cnt = 0
 for _ in range(m):
     a, b, c = map(int, input().split())
-    if a == 1:
-        cnt += 1
     graph.append((a, b, c))
 
 INF = sys.maxsize
-time = [INF] * (n+1)
-
-def belmanford(start_node):
-    time[start_node] = 0
+def bellmanford(start):
+    distance = [INF] * (n+1)
+    distance[start] = 0
 
     for i in range(n):
-        for start, end, weight in graph:
-            if time[start] + weight < time[end]:
-                time[end] = time[start] + weight
+        for j in range(m):
+            cur_node, next_node, cur_weight = graph[j]
+            if distance[cur_node] == INF:
+                continue
+            if distance[cur_node] + cur_weight < distance[next_node]:
+                distance[next_node] = distance[cur_node] + cur_weight
                 if i == n - 1:
-                    return False
+                    return [-1]
     
-    return True
+    for i in range(n+1):
+        if distance[i] == INF:
+            distance[i] = -1
+    
+    return distance[2:]
 
-if cnt == 0:
-    for _ in range(n-1):
-        print(-1)
-    exit()
+print(*bellmanford(1), sep='\n')
 
-if belmanford(1):
-    for i in range(2, n+1):
-        if time[i] == sys.maxsize:
-            print(-1)
-        else:
-            print(time[i])
-else:
-    print(-1)
