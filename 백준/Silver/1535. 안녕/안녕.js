0cbdb542e990e17ee23d9359e52ff32abba1a2a6
A_jin <77830408+ppochaco@users.noreply.github.com>
2026-04-08 22:54:31 +0900

    [Silver II] Title: 안녕, Time: 104 ms, Memory: 9356 KB -BaekjoonHub

--- 백준/Silver/1535. 안녕/안녕.js
+++ 백준/Silver/1535. 안녕/안녕.js
@@ -1,19 +1,15 @@
-const input = require("fs")
-  .readFileSync(
-    process.platform === "linux" ? "/dev/stdin" : __dirname + "/input.txt"
-  )
-  .toString()
-  .split("\n");
+const input = require('fs').readFileSync(process.platform === 'linux' ? '/dev/stdin' : __dirname + '/input.txt').toString().split('\n')
 
-const n = Number(input.shift());
-const l = input.shift().split(" ").map(Number);
-const j = input.shift().split(" ").map(Number);
-const life = Array.from({ length: 100 + 1 }, () => 0);
+const N = Number(input[0])
+const people = []
+people.push(input[1].split(' ').map(Number))
+people.push(input[2].split(' ').map(Number))
 
-for (let i = 0; i < n; i++) {
-  for (let cur_life = 100; cur_life - l[i] > 0; cur_life--) {
-    life[cur_life] = Math.max(life[cur_life], life[cur_life - l[i]] + j[i]);
+const dp = Array(100).fill(0)
+for (let i = 0; i < N; i++) {
+  const [hp, joy] = [people[0][i], people[1][i]]
+  for (let j = 99; j >= hp; j--) {
+    dp[j] = Math.max(dp[j], dp[j - hp] + joy)
   }
 }
-
-console.log(life[100]);
+console.log(dp[99])
