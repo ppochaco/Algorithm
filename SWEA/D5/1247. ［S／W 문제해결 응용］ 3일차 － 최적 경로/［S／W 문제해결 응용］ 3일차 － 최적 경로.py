84b33bc1087647404837699fbe471923065802a9
A_jin <77830408+ppochaco@users.noreply.github.com>
2026-05-18 16:16:41 +0900

    [D5] Title: [S/W 문제해결 응용] 3일차 - 최적 경로, Time: 134 ms, Memory: 62,592 KB -BaekjoonHub

--- SWEA/D5/1247. ［S／W 문제해결 응용］ 3일차 － 최적 경로/［S／W 문제해결 응용］ 3일차 － 최적 경로.py
+++ SWEA/D5/1247. ［S／W 문제해결 응용］ 3일차 － 최적 경로/［S／W 문제해결 응용］ 3일차 － 최적 경로.py
@@ -1,48 +1,44 @@
 T = int(input())
+INF = float('inf')
 
-def calculate_distance(a, b):
+def manhattan(a, b):
   return abs(a[0] - b[0]) + abs(a[1] - b[1])
-
-def dfs(depth, pre, distance):
-  global answer
-
-  if depth == N + 1:
-    dis = calculate_distance(arr[pre], home)
-    answer = min(answer, distance + dis)
-    return
-  
-  if answer < distance:
-    return
-  
-  for cur in range(N + 1):
-    if visited[cur]:
-      continue
-
-    dis = calculate_distance(arr[cur], arr[pre])
-
-    visited[cur] = True
-    distance += dis
-
-    dfs(depth + 1, cur, distance)
-    
-    visited[cur] = False
-    distance -= dis
-
 
 for test_case in range(1, T + 1):
   N = int(input())
   temp = list(map(int, input().split()))
-  work = [temp[0], temp[1]]
+
+  company = [temp[0], temp[1]]
   home = [temp[2], temp[3]]
-  customer = []
+  customers = []
   for i in range(4, N * 2 + 4, 2):
-    customer.append([temp[i], temp[i + 1]])
+    customers.append([temp[i], temp[i + 1]])
+
+  dp = [[INF] * N for _ in range(1 << N)]
+
+  for i in range(N):
+    dp[1 << i][i] = manhattan(company, customers[i])
+
+  for mask in range(1 << N):
+    for i in range(N):
+      if not (mask & (1 << i)):
+        continue
+      if dp[mask][i] == INF:
+        continue
+
+      for j in range(N):
+        if mask & (1 << j):
+          continue
+
+        new_mask = mask | (1 << j)
+        dist = manhattan(customers[i], customers[j])
+        dp[new_mask][j] = min(dp[new_mask][j], dp[mask][i] + dist)
   
-  answer = 100 * 12
-  visited = [False for _ in range(N + 1)]
-  arr = [work] + customer
-  
-  visited[0] = True
-  dfs(1, 0, 0)
+  full_mask = (1 << N) - 1
+  answer = INF
+
+  for i in range(N):
+    result = dp[full_mask][i] + manhattan(customers[i], home)
+    answer = min(answer, result)
 
   print(f"#{test_case} {answer}")
