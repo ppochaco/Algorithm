694046ad89f246c94d4a0c69573cad9177edffb6
A_jin <77830408+ppochaco@users.noreply.github.com>
2026-05-18 14:26:38 +0900

    [D5] Title: [S/W 문제해결 응용] 3일차 - 최적 경로, Time: 829 ms, Memory: 65,024 KB -BaekjoonHub

--- SWEA/D5/1247. ［S／W 문제해결 응용］ 3일차 － 최적 경로/［S／W 문제해결 응용］ 3일차 － 최적 경로.py
+++ SWEA/D5/1247. ［S／W 문제해결 응용］ 3일차 － 최적 경로/［S／W 문제해결 응용］ 3일차 － 최적 경로.py
@@ -1,23 +1,48 @@
-from itertools import permutations
-
 T = int(input())
+
+def calculate_distance(a, b):
+  return abs(a[0] - b[0]) + abs(a[1] - b[1])
+
+def dfs(depth, pre, distance):
+  global answer
+
+  if depth == N + 1:
+    dis = calculate_distance(arr[pre], home)
+    answer = min(answer, distance + dis)
+    return
+  
+  if answer < distance:
+    return
+  
+  for cur in range(N + 1):
+    if visited[cur]:
+      continue
+
+    dis = calculate_distance(arr[cur], arr[pre])
+
+    visited[cur] = True
+    distance += dis
+
+    dfs(depth + 1, cur, distance)
+    
+    visited[cur] = False
+    distance -= dis
+
 
 for test_case in range(1, T + 1):
   N = int(input())
-  arr = list(map(int, input().split()))
-  work = [arr[0], arr[1]]
-  home = [arr[2], arr[3]]
+  temp = list(map(int, input().split()))
+  work = [temp[0], temp[1]]
+  home = [temp[2], temp[3]]
   customer = []
   for i in range(4, N * 2 + 4, 2):
-    customer.append([arr[i], arr[i + 1]])
+    customer.append([temp[i], temp[i + 1]])
   
   answer = 100 * 12
-  for cur in permutations(customer):
-    distance = 0
-    arr = [work] + list(cur) + [home]
-    for i in range(1, N + 2):
-      distance += abs(arr[i - 1][0] - arr[i][0]) + abs(arr[i - 1][1] - arr[i][1])
-
-    answer = min(answer, distance)
+  visited = [False for _ in range(N + 1)]
+  arr = [work] + customer
+  
+  visited[0] = True
+  dfs(1, 0, 0)
 
   print(f"#{test_case} {answer}")
