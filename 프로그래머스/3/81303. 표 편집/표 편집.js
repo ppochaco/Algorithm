6668c9a627e860205a0372846c6a84b82f4669f6
A_jin <77830408+ppochaco@users.noreply.github.com>
2026-07-15 16:31:26 +0900

    [level 3] Title: 표 편집, Time: 246.60 ms, Memory: 193 MB -BaekjoonHub

--- 프로그래머스/3/81303. 표 편집/표 편집.js
+++ 프로그래머스/3/81303. 표 편집/표 편집.js
@@ -6,103 +6,76 @@ class Node {
     }
 }
 
-class LinkedList {
-    constructor(n, k) {
-        this.list = []
-        this.point = null
-        this.stack = []
-        this.#init(n, k)
-    }
-    
-    #init(n, k) {
-        for (let i = 0; i < n; i++) {
-            this.list.push(new Node(i))
-        }
-        
-        for (let i = 0; i < n; i++) {
-            if (0 < i) {
-                this.list[i].prev = this.list[i - 1]
-            }
-            if (i < n - 1) {
-                this.list[i].next = this.list[i + 1]
-            }
-        }
-        
-        this.point = this.list[k]
-    }
-    
-    up(x) {
-        for (let i = 0; i < x; i++) {
-            this.point = this.point.prev
-        }
-    }
-    
-    down(x) {
-        for (let i = 0; i < x; i++) {
-            this.point = this.point.next
-        }
-    }
-    
-    clear() {
-        const node = this.point
-        
-        this.stack.push(node)
-        
-        if (node.prev) {
-            node.prev.next = node.next
-        }
-        if (node.next) {
-            node.next.prev = node.prev
-        }
-        
-        if (node.next) {
-            this.point = node.next
-        } else {
-            this.point = node.prev
-        } 
-    }
-    
-    restore() {
-        if (!this.stack.length) return
-        
-        const node = this.stack.pop()
-        if (node.prev) {
-            node.prev.next = node
-        }
-        if (node.next) {
-            node.next.prev = node
-        }
-    }
-    
-    getList(n) {
-        const result = new Array(n).fill('O')
-        
-        for (const node of this.stack) {
-            result[node.data] = 'X'
-        }
-        
-        return result.join('')
-    }
-}
-
 function solution(n, k, cmd) {
-    const linkedList = new LinkedList(n, k)
+    const list = []
+    const stack = []
+    let point = null
+    
+    for (let i = 0; i < n; i++) {
+        list.push(new Node(i))
+    }
+
+    for (let i = 0; i < n; i++) {
+        if (0 < i) {
+            list[i].prev = list[i - 1]
+        }
+        if (i < n - 1) {
+            list[i].next = list[i + 1]
+        }
+    }
+
+    point = list[k]
 
     cmd.forEach(c => {
         const [command, number] = c.split(' ')
+        
         if (command === 'U') {
-            linkedList.up(number)
+            for (let i = 0; i < number; i++) {
+                point = point.prev
+            }
         }
+        
         if (command === 'D') {
-            linkedList.down(number)
+            for (let i = 0; i < number; i++) {
+                point = point.next
+            }
         }
+        
         if (command === 'C') {
-            linkedList.clear()
+            const node = point
+        
+            stack.push(node)
+
+            if (node.prev) {
+                node.prev.next = node.next
+            }
+            if (node.next) {
+                node.next.prev = node.prev
+            }
+
+            if (node.next) {
+                point = node.next
+            } else {
+                point = node.prev
+            } 
         }
+        
         if (command === 'Z') {
-            linkedList.restore()
+            const node = stack.pop()
+            if (node.prev) {
+                node.prev.next = node
+            }
+            if (node.next) {
+                node.next.prev = node
+            }
         }
     })
     
-    return linkedList.getList(n);
+    const result = new Array(n).fill('O')
+        
+    for (const node of stack) {
+        result[node.data] = 'X'
+    }
+    
+    return result.join('');
 }
