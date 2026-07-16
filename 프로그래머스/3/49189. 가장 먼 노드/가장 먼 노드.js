19103a8d5c63458c93c8a1ab61913fe3de29516d
A_jin <77830408+ppochaco@users.noreply.github.com>
2026-07-16 15:49:02 +0900

    [level 3] Title: 가장 먼 노드, Time: 14.61 ms, Memory: 74.4 MB -BaekjoonHub

---
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.js
@@ -0,0 +1,28 @@
+function solution(n, edge) {
+    const graph = Array.from({ length: n + 1 }, () => [])
+    for (const [a, b] of edge) {
+        graph[a].push(b)
+        graph[b].push(a)
+    }
+    
+    const visited = new Array(n + 1).fill(0)
+    const queue = [1]
+    let index = 0
+    while (index < queue.length) {
+        const node = queue[index++]
+        for (const next_node of graph[node]) {
+            if (visited[next_node]) continue
+            queue.push(next_node)
+            visited[next_node] = visited[node] + 1
+        }
+    }
+    visited[1] = 0
+    
+    const max_distance = Math.max(...visited)
+    let answer = 0
+    for (let i = 1; i <= n; i++) {
+        if (max_distance === visited[i]) answer++
+    }
+    
+    return answer;
+}
